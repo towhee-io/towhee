b55467ff5edff1502c7486302c5353ea7c69bac2
Kaiyuan Hu <ucapkhu@ucl.ac.uk>
2021-08-28 13:44:13 +0800

    delete unnecessary code in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ import unittest
 def test_suite():
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover('towhee/tests', pattern='test_*.py')
-    # test_suite = test_loader.discover('towhee/tests/compiler/utils', pattern='test_*.py')
     return test_suite
 
 setup(
