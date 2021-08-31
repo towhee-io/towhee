6747ea57d5609d9b7026669201234549fe7f7321
derekdqc <578093244@qq.com>
2021-08-31 15:28:57 +0800

    add lisence and revise docstring

--- towhee/data/image_datasets.py
+++ towhee/data/image_datasets.py
@@ -1,5 +1,19 @@
-import os
+# coding=utf-8
+# Copyright 2020-present the HuggingFace Inc. team and 2021 Zilliz.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+import os
 import pandas as pd
 from pandas import Series
 from torch.utils.data import Dataset
@@ -8,16 +22,16 @@ from PIL import Image
 
 class ImageDataset(Dataset):
     """
-        PytorchImageDataset is a dataset class for PyTorch.
+        ImageDataset is a dataset class for PyTorch.
 
         Args:
             image_path (:obj:`str`):
                 Path to the images for your dataset.
 
             label_file (:obj:`str`):
-                 Path to your label file. The label file should be a csv file. The columns in this file should be [image_name, category],
-                'image_name' is the path of your images, 'category' is the label of accordance image. For example:
-                [image_name, dog] for one row. Note that the first row should be[image_name, category]
+                 Path to your label file. The label file should be a csv file. The columns in this file should be
+                 [image_name, category], 'image_name' is the path of your images, 'category' is the label of accordance
+                 image. For example: [image_name, dog] for one row. Note that the first row should be[image_name, category]
 
             data_transform (:obj:`Compose`):
                 PyTorch transform of the input images.
