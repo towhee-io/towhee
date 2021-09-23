01d757275a442cf1f0b6e81cd1f5c70a1bb7e5ae
derekdqc <578093244@qq.com>
2021-09-23 16:27:01 +0800

    callback impl and test.

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'pandas>=1.2.4',
         'pyyaml>=5.3',
         'requests>=2.25',
+        'tqdm>=4.59.0'
     ],
     packages=find_packages(),
     license="http://www.apache.org/licenses/LICENSE-2.0"
