046a8598f708111e69d3f3a6e085b19de66305ab
oneseer <hovmey@hotmail.com>
2021-09-23 11:10:30 +0800

    add transformer.py for transformer modelling add test_transformer.py for transformer ut

---
+++ towhee/trainer/models/vit/transformer.py
@@ -0,0 +1,128 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Adapted from https://github.com/lukemelas/simple-bert
+"""
+
+import numpy as np
+from torch import nn
+from torch.nn import functional as F
+
+
+def split_last(x, shape):
+    """
+    split the last dimension to given shape
+    """
+    shape = list(shape)
+    assert shape.count(-1) <= 1
+    if -1 in shape:
+        shape[shape.index(-1)] = int(x.size(-1) / -np.prod(shape))
+    return x.view(*x.size()[:-1], *shape)
+
+
+def merge_last(x, n_dims):
+    """
+    Merge the last n_dims to a dimension
+    """
+    s = x.size()
+    assert n_dims > 1
+    assert n_dims < len(s)
+    return x.view(*s[:-n_dims], -1)
+
+
+class MultiHeadedSelfAttention(nn.Module):
+    """
+    Multi-Headed Dot Product Attention
+    """
+    def __init__(self, dim, num_heads, dropout):
+        super().__init__()
+        self.proj_q = nn.Linear(dim, dim)
+        self.proj_k = nn.Linear(dim, dim)
+        self.proj_v = nn.Linear(dim, dim)
+        self.drop = nn.Dropout(dropout)
+        self.n_heads = num_heads
+        self.scores = None # for visualization
+
+    def forward(self, x, mask):
+        """
+        x, q(query), k(key), v(value) : (B(batch_size), S(seq_len), D(dim))
+        mask : (B(batch_size) x S(seq_len))
+        * split D(dim) into (H(n_heads), W(width of head)) ; D = H * W
+        """
+        # (B, S, D) -proj-> (B, S, D) -split-> (B, S, H, W) -trans-> (B, H, S, W)
+        q, k, v = self.proj_q(x), self.proj_k(x), self.proj_v(x)
+        q, k, v = (split_last(x, (self.n_heads, -1)).transpose(1, 2) for x in [q, k, v])
+        # (B, H, S, W) @ (B, H, W, S) -> (B, H, S, S) -softmax-> (B, H, S, S)
+        scores = q @ k.transpose(-2, -1) / np.sqrt(k.size(-1))
+        if mask is not None:
+            mask = mask[:, None, None, :].float()
+            scores -= 10000.0 * (1.0 - mask)
+        scores = self.drop(F.softmax(scores, dim=-1))
+        # (B, H, S, S) @ (B, H, S, W) -> (B, H, S, W) -trans-> (B, S, H, W)
+        h = (scores @ v).transpose(1, 2).contiguous()
+        # -merge-> (B, S, D)
+        h = merge_last(h, 2)
+        self.scores = scores
+        return h
+
+
+class PositionWiseFeedForward(nn.Module):
+    """
+    FeedForward Neural Networks for each position
+    """
+    def __init__(self, dim, ff_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(dim, ff_dim)
+        self.fc2 = nn.Linear(ff_dim, dim)
+
+    def forward(self, x):
+        # (B, S, D) -> (B, S, D_ff) -> (B, S, D)
+        return self.fc2(F.gelu(self.fc1(x)))
+
+
+class Block(nn.Module):
+    """
+    Transformer Block
+    """
+    def __init__(self, dim, num_heads, ff_dim, dropout):
+        super().__init__()
+        self.attn = MultiHeadedSelfAttention(dim, num_heads, dropout)
+        self.proj = nn.Linear(dim, dim)
+        self.norm1 = nn.LayerNorm(dim, eps=1e-6)
+        self.pwff = PositionWiseFeedForward(dim, ff_dim)
+        self.norm2 = nn.LayerNorm(dim, eps=1e-6)
+        self.drop = nn.Dropout(dropout)
+
+    def forward(self, x, mask):
+        h = self.drop(self.proj(self.attn(self.norm1(x), mask)))
+        x = x + h
+        h = self.drop(self.pwff(self.norm2(x)))
+        x = x + h
+        return x
+
+
+class Transformer(nn.Module):
+    """
+    Transformer with Self-Attentive Blocks
+    """
+    def __init__(self, num_layers, dim, num_heads, ff_dim, dropout):
+        super().__init__()
+        self.blocks = nn.ModuleList([
+            Block(dim, num_heads, ff_dim, dropout) for _ in range(num_layers)])
+
+    def forward(self, x, mask=None):
+        for block in self.blocks:
+            x = block(x, mask)
+        return x
