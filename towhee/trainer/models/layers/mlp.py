8e6b13a8a621aad900fac5b77ff5425c50cb4357
oneseer <hovmey@hotmail.com>
2021-10-18 15:24:31 +0800

    add MLP layer and its ut

---
+++ towhee/trainer/models/layers/mlp.py
@@ -0,0 +1,80 @@
+# Copyright 2021 Zilliz and Facebook. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import torch
+import torch.nn as nn
+from typing import Callable, List, Optional, Tuple
+
+
+class MLP(nn.Module):
+    """
+    A MLP block that contains two linear layers with a normalization layer.
+    The MLP block is used in a transformer model after the attention block.
+    ::
+                         Linear (in_features, hidden_features)
+                                           ↓
+                                 Normalization (act_layer)
+                                           ↓
+                                Dropout (p=dropout_rate)
+                                           ↓
+                         Linear (hidden_features, out_features)
+                                           ↓
+                                Dropout (p=dropout_rate)
+    Args:
+        in_features(int):
+            Input feature dimension.
+        hidden_features(optional, int):
+            Hidden feature dimension. By default, hidden feature is set to input feature dimension.
+        out_features(optional, int):
+            Output feature dimension. By default, output
+            features dimension is set to input feature dimension.
+        act_layer(Callable):
+            Activation layer used after the first linear layer.
+        dropout_rate(float):
+            Dropout rate after each linear layer. Dropout is not used by default.
+    """
+
+    def __init__(
+        self,
+        in_features: int,
+        hidden_features: Optional[int] = None,
+        out_features: Optional[int] = None,
+        act_layer: Callable = nn.GELU,
+        dropout_rate: float = 0.0,
+    ) -> None:
+        super().__init__()
+        self.dropout_rate = dropout_rate
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        if self.dropout_rate > 0.0:
+            self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x(tensor):
+                Input tensor.
+        """
+        x = self.fc1(x)
+        x = self.act(x)
+        if self.dropout_rate > 0.0:
+            x = self.dropout(x)
+        x = self.fc2(x)
+        if self.dropout_rate > 0.0:
+            x = self.dropout(x)
+        return x
