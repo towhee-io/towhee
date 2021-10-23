59b15c1a4d2874f6318f8b06170c6b50b16c23a1
wxywb <xy.wang@zilliz.com>
2021-10-23 16:32:50 +0800

    add activation layers.

---
+++ towhee/models/layers/activations/hardswish.py
@@ -0,0 +1,42 @@
+# Copyright 2021 Ross Wightman and Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+def hard_swish(x: torch.Tensor, inplace: bool = False) -> torch.Tensor:
+    inner = F.relu6(x + 3.).div_(6.)
+    return x.mul_(inner) if inplace else x.mul(inner)
+
+class HardSwish(nn.Module):
+    """
+    HardSwish activiation layer.
+
+    Applies the hardswish function, element-wise.
+    Described in: https://arxiv.org/abs/1905.02244.
+
+    Args:
+        inplace(`Bool`):
+            whether use inplace version.
+    Returns:
+        (`torch.Tensor`)
+            output tensor after activation.
+    """
+    def __init__(self, inplace: bool = False) -> None:
+        super().__init__()
+        self.inplace = inplace
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return hard_swish(x, self.inplace)
