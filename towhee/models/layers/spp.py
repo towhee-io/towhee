37e5c82bb139cb056a038ebc9f5723a8adcc954e
NbnbZero <chenling9727@gmail.com>
2021-10-27 15:34:28 +0800

    Update spp.py

--- towhee/models/layers/spp.py
+++ towhee/models/layers/spp.py
@@ -11,24 +11,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import math
 import torch
-
 from torch import nn
 
 
 class SPP(nn.Module):
     """
     Spatial Pyramid Pooling in Deep Convolutional Networks
-    """
-    def __init__(self, kernel_size1, kernel_size2, kernel_size3):
-        super().__init__()
-        self.pool1 = nn.MaxPool2d(kernel_size1, stride=1, padding=kernel_size1 // 2)
-        self.pool2 = nn.MaxPool2d(kernel_size2, stride=1, padding=kernel_size2 // 2)
-        self.pool3 = nn.MaxPool2d(kernel_size3, stride=1, padding=kernel_size3 // 2)
+    pre_conv: a tensor vector of previous convolution layer
+    pre_conv_size: an int vector [height, width] of the matrix features size of previous convolution layer
+    out_pool_size: a int vector of expected output size of max pooling layer
 
-    def forward(self, x):
-        x1 = self.pool1(x)
-        x2 = self.pool2(x)
-        x3 = self.pool3(x)
-        return torch.cat([x, x1, x2, x3], dim=1)
+    returns: a tensor vector with shape [1 x n] is the concentration of multi-level pooling
+    """
+    def __init__(self, pre_conv, pre_conv_size, out_pool_size):
+        super().__init__()
+        sample_num = pre_conv.shape[0]
+        for i in range(len(out_pool_size)):
+            height = int(math.ceil(pre_conv_size[0] / out_pool_size[i]))
+            width = int(math.ceil(pre_conv_size[1] / out_pool_size[i]))
+            h_pad = (height * out_pool_size[i] - pre_conv_size[0] + 1) / 2
+            w_pad = (width * out_pool_size[i] - pre_conv_size[1] + 1) / 2
+            maxpool = torch.nn.MaxPool2d((height, width), stride=(height, width), padding=(int(h_pad), int(w_pad)))
+            x = maxpool(pre_conv)
+            if (i == 0):
+                self.spp = x.view(sample_num, -1)
+            else:
+                self.spp = torch.cat((self.spp, x.view(sample_num, -1)), 1)
+
+    def forward(self):
+        return self.spp
