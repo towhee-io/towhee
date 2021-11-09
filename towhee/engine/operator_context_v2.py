aa0b6c77103afb7bbabf79e7f12619c05a494cbf
junjiejiangjjj <junjie.jiang@zilliz.com>
2021-11-09 23:59:07 +0800

    Operator ctx

---
+++ towhee/engine/operator_context_v2.py
@@ -0,0 +1,108 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict, Tuple
+from enum import Enum, auto
+
+from towhee.dag.operator_repr import OperatorRepr
+from towhee.engine.operator_runner.runner_base import RunnerStatus
+from towhee.dataframe import DataFrame
+from towhee.engine.operator_io import create_reader, create_writer
+from towhee.engine.operator_runner import create_runner
+from towhee.engine.task_executor import TaskExecutor
+
+
+class OpStatus(Enum):
+    NOT_RUNNING = auto()
+    RUNNING = auto()
+    FINISHED = auto()
+    FAILED = auto()
+    STOPPED = auto()
+
+
+class OperatorContext:
+    """
+    The OperatorContext manages an operator's input data and output data at runtime,
+    as well as the operators' dependency within a GraphContext.
+    The abstraction of OperatorContext hides the complexity of Dataframe management,
+    input iteration, and data dependency between Operators. It offers a Task-based
+    scheduling context.
+
+    Args:
+        op_repr: (OperatorRepr)
+            The operator representation
+        dataframes: (`dict` of `DataFrame`)
+            All the `DataFrames` in `GraphContext`
+    """
+
+    def __init__(
+        self,
+        op_repr: OperatorRepr,
+        dataframes: Dict[str, DataFrame]
+    ):
+        self._repr = op_repr
+
+        inputs = list({dataframes[input['df']] for input in op_repr.inputs})
+        iter_type = op_repr.iter_info['type']
+        inputs_index = dict((item['name'], item['col'])
+                            for item in op_repr.inputs)
+        self.inputs = inputs
+        self._reader = create_reader(inputs, iter_type, inputs_index)
+
+        outputs = list({dataframes[output['df']]
+                       for output in op_repr.outputs})
+
+        self._writer = create_writer(iter_type, outputs)
+        self._op_runners = []
+        self._op_status = OpStatus.NOT_RUNNING
+
+    @property
+    def name(self):
+        return self._repr.name
+
+    def status(self):
+        """
+        Calc op-ctx status by checking all runners of this op-ctx
+        """
+        if self._op_status == OpStatus.FINISHED:
+            return self._op_status
+
+        finished_count = 0
+        for runner in self._op_runners:
+            if runner.status == RunnerStatus.FAILED:
+                self._op_status = OpStatus.FAILED
+            else:
+                if runner.status in [RunnerStatus.FINISHED, RunnerStatus.STOPPED]:
+                    finished_count += 1
+        if finished_count == len(self._op_runners):
+            self._op_status = OpStatus.FINISHED
+        return self._op_status
+
+    def start(self, count, executor: TaskExecutor) -> None:
+        if self._op_status != OpStatus.NOT_RUNNING:
+            raise RuntimeError("OperatorContext can only be started once")
+
+        self._op_status == OpStatus.RUNNING
+        
+        for i in range(count):
+            self._op_runners.append(
+                create_runner(self.op_repr.name, i, self._reader, self._writer)
+            )
+        for runner in self._op_runners:
+            executor.add(runner)
+
+    def stop(self):
+        for runner in self._op_runners:
+            runner.set_stop()
+        self._status = OpStatus.STOPPED
---
+++ towhee/engine/operator_io/writer.py
@@ -0,0 +1,35 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from towhee.dataframe import DataFrame, Variable, DataFrameIterator
+from typing import Dict, Optional, Tuple, Union, List, NamedTuple
+
+
+class DataFrameWriter:
+    """
+    Df writer
+    """
+
+    def __init__(self, output_df: DataFrame):
+        self._output_df = output_df
+
+    def write(self, output_data: NamedTuple) -> bool:
+
+        # TODO (jiangjunjie) deal with task exception
+        if output_data is not None:
+            return self._output_df.put_dict(output_data._asdict())
+
+    def close(self):
+        self._output_df.seal()
