5735e409d9d644996a53afbb145dd46003299bb8
binbin <83755740+binbinlv@users.noreply.github.com>
2021-12-07 14:38:15 +0800

    Add test cases (#310)
    Signed-off-by: Binbin Lv <binbin.lv@zilliz.com>

--- tests/testcases/test_image_object_embedding.py
+++ tests/testcases/test_image_object_embedding.py
@@ -4,14 +4,15 @@ sys.path.append("../../tests")
 import time
 import threading
 from towhee import pipeline
-from common import common_func as cf
+
+data_path = "../data_set/object_image/"
 
 embedding_size = 1000
 
-class TestImageEmbeddingInvalid:
+class TestImageObjEmbeddingInvalid:
     """ Test case of invalid embedding interface """
 
-    def test_embedding_no_parameter(self, pipeline_name):
+    def test_embedding_no_image(self, pipeline_name):
         """
         target: test embedding for invalid scenario
         method:  embedding with no image, now issue 154
@@ -25,67 +26,47 @@ class TestImageEmbeddingInvalid:
 
         return True
 
-    def test_embedding_wrong_parameter(self, pipeline_name):
+    def test_embedding_no_obj_image(self, pipeline_name):
         """
-        target: test embedding for invalid scenario
-        method:  embedding with no image, now issue 154
-        expected: raise exception
+        target: test embedding for no object image
+        method:  embedding with no object image
+        expected: raise exception for empty result
         """
+        img = "../../docs/towhee_logo.png"
         embedding_pipeline = pipeline(pipeline_name)
         try:
-            embedding = embedding_pipeline("")
+            embedding = embedding_pipeline(img)
         except RuntimeError as e:
-            assert "'str' object has no attribute 'read'" in str(e)
+            assert "Unkown error" in str(e)
 
         return True
 
-    def test_embedding_not_exist_image(self, pipeline_name):
-        """
-        target: test embedding for invalid scenario
-        method:  embedding with no image, now issue 154
-        expected: raise exception
-        """
-        embedding_pipeline = pipeline(pipeline_name)
-        not_exist_img = "towhee_no_exist.jpg"
-        try:
-            embedding = embedding_pipeline(not_exist_img)
-        except RuntimeError as e:
-            assert "No such file or directory" in str(e)
-
-        return True
-
-    def test_embedding_image_list(self, pipeline_name):
-        """
-        target: test embedding with image list
-        method:  embedding with image list
-        expected: raise exception
-        """
-        N = 3
-        img_list = []
-        img = cf.create_image()
-        for i in range(N):
-            img_list.append(img)
-        embedding_pipeline = pipeline(pipeline_name)
-        try:
-            embedding = embedding_pipeline(img_list)
-        except RuntimeError as e:
-            assert "img should be PIL Image" in str(e)
-
-        return True
-
-class TestImageEmbeddingValid:
+class TestImageObjEmbeddingValid:
     """ Test case of valid embedding interface """
 
-    def test_embedding_one_image(self, pipeline_name):
+    def test_embedding_one_image_single_obj(self, pipeline_name):
         """
         target: test embedding for normal case
         method:  embedding with one image
         expected: return embeddings
         """
-        img = cf.create_image()
         embedding_pipeline = pipeline(pipeline_name)
-        embedding = embedding_pipeline(img)
+        embedding = embedding_pipeline(data_path + "towhee_test_image0.jpg")
         assert embedding[0][0].size == embedding_size
+
+        return True
+
+    def test_embedding_one_image_multiple_obj(self, pipeline_name):
+        """
+        target: test embedding for normal case
+        method:  embedding with one image
+        expected: return embeddings
+        """
+        embedding_pipeline = pipeline(pipeline_name)
+        embedding = embedding_pipeline(data_path + "towhee_test_image1.jpg")
+        assert embedding[0][0].size == embedding_size
+        assert embedding[1][0].size == embedding_size
+        assert len(embedding) == 2
 
         return True
 
@@ -95,13 +76,10 @@ class TestImageEmbeddingValid:
         method:  embedding with same images
         expected: return identical embeddings
         """
-        img = cf.create_image()
-        img_1 = img.copy()
         embedding_pipeline = pipeline(pipeline_name)
-        embedding = embedding_pipeline(img)
-        embedding1 = embedding_pipeline(img_1)
+        embedding = embedding_pipeline(data_path + "towhee_test_image0.jpg")
+        embedding1 = embedding_pipeline(data_path + "towhee_test_image2.jpg")
         assert embedding[0][0].size == embedding_size
-        assert embedding1[0][0].size == embedding_size
         assert embedding[0][0].all() == embedding1[0][0].all()
 
         return True
@@ -116,9 +94,8 @@ class TestImageEmbeddingValid:
         embeddings = []
         embedding_pipeline = pipeline(pipeline_name)
         for i in range(nums):
-            img = cf.create_image()
-            embedding = embedding_pipeline(img)
-            embeddings.append(embedding[0][0])
+            embedding = embedding_pipeline(data_path + "towhee_test_image%i.jpg" % i)
+            embeddings.append(embedding)
             assert embedding[0][0].size == embedding_size
 
         assert embeddings[0][0] is not embeddings[1][0]
@@ -132,13 +109,12 @@ class TestImageEmbeddingValid:
         expected: return identical embeddings
         """
         nums = 10
-        img = cf.create_image()
         embedding_pipeline = pipeline(pipeline_name)
-        embedding_last = embedding_pipeline(img)
+        embedding_last = embedding_pipeline(data_path + "towhee_test_image0.jpg")
         for _ in range(nums):
-            embedding = embedding_pipeline(img)
+            embedding = embedding_pipeline(data_path + "towhee_test_image0.jpg")
             assert embedding[0][0].size == embedding_size
-            assert embedding[0][0].all() == embedding_last[0][0].all()
+        assert embedding[0][0].all() == embedding_last[0][0].all()
 
         return True
 
@@ -154,8 +130,7 @@ class TestImageEmbeddingValid:
         for _ in range(times):
             embeddings = []
             for i in range(nums):
-                img = cf.create_image()
-                embedding = embedding_pipeline(img)
+                embedding = embedding_pipeline(data_path + "towhee_test_image0.jpg")
                 embeddings.append(embedding)
                 assert embedding[0][0].size == embedding_size
             assert embeddings[0][0] is not embeddings[1][0]
@@ -170,11 +145,10 @@ class TestImageEmbeddingValid:
         """
         threads_num = 10
         threads = []
-        img = cf.create_image()
 
         def embedding():
             embedding_pipeline = pipeline(pipeline_name)
-            embedding = embedding_pipeline(img)
+            embedding = embedding_pipeline(data_path + "towhee_test_image0.jpg")
             assert embedding[0][0].size == embedding_size
 
         for i in range(threads_num):
