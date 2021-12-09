ee3a17915d512d8864524eb03675b717c28af6de
Binbin Lv <binbin.lv@zilliz.com>
2021-12-09 16:25:40 +0800

    Update towhee version
    Signed-off-by: Binbin Lv <binbin.lv@zilliz.com>

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def parse_requirements(file_name: str) -> List[str]:
 
 setup(
     name="towhee",
-    version="0.2.1",
+    version="0.3.0",
     description="",
     author="Towhee Team",
     author_email="towhee-team@zilliz.com",
