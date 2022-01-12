4aa3247191b1cc749c9e2afc76d61c2ff92b5bf9
shiyu22 <shiyu.chen@zilliz.com>
2022-01-12 20:08:07 +0800

    Update main for hub tool
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>

--- setup.py
+++ setup.py
@@ -49,4 +49,9 @@ setup(
     packages=find_packages(),
     package_data={'towhee.tests.test_util': ['*.yaml']},
     license="http://www.apache.org/licenses/LICENSE-2.0",
+    entry_points={
+        'console_scripts': [
+            'towheehub=towhee.hub.bin.hub_tools:main'
+        ],
+    }
 )
