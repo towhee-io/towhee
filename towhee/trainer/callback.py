209fcc3e000d4a6dbd0103038191318c9c8423ee
wxywb <xy.wang@zilliz.com>
2022-02-16 17:44:58 +0800

    add earlystopping and modelcheckpoint callback.

--- towhee/trainer/callback.py
+++ towhee/trainer/callback.py
@@ -15,6 +15,7 @@
 
 from typing import Dict, Tuple, List
 
+import numpy as np
 from torch import nn
 from torch.optim import Optimizer
 
@@ -209,4 +210,107 @@ class CallbackList:
         for cb in self.callbacks:
             cb.on_eval_end(logs)
 
+class EarlyStopping(Callback):
+    """
+    EarlyStopping
+    """
+    def __init__(self,
+                 monitor,
+                 min_delta = 0,
+                 patience = 0,
+                 mode = 'max',
+                 baseline = None
+                 ):
+        #super(EarlyStopping, self).__init__()
+        super(EarlyStopping).__init__()
+        self.monitor = monitor
+        self.patience = patience
+        #self.verbose = verbose
+        self.baseline = baseline
+        self.min_delta = abs(min_delta)
+        self.wait = 0
+        self.stopped_epoch = 0
+        self.best_weights = None
+
+        assert mode in ['max', 'min']
+
+        if mode == 'min':
+            self.monitor_op = np.less
+        else:
+            self.monitor_op = np.greater
+
+        if self.monitor_op == np.greater:
+            self.min_delta *= 1
+        else:
+            self.min_delta *= -1
+
+    def on_train_begin(self, logs = None):
+        self.wait = 0
+        self.stopped_epoch = 0
+        self.best = np.Inf if self.monitor_op == np.less else -np.Inf
+        self.best_weights = None
+        self.best_epoch = 0
+
+    def on_epoch_end(self, epochs, logs = None):
+        current = self.get_monitor_value(logs)
+        if current is None:
+            return
+        #if self.restore_best_weights and self.best_weights is None:
+        #    self.best_weights = self.model.get_weights()
+
+        self.wait += 1
+        if self._is_improvement(current, self.best):
+            self.best = current
+            self.best_epoch = epochs
+           # if self.restore_best_weights:
+           #     self.best_weights = self.model.get_weights()
+            if self.baseline is None or self._is_improvement(current, self.baseline):
+                self.wait = 0
+
+        if self.wait >= self.patience and epochs > 0:
+            self.stopped_epoch = epochs
+
+    def on_train_end(self, logs = None):
+        #TODO
+        pass
+
+    def get_monitor_value(self, logs):
+        logs = logs or {}
+        monitor_value = logs.get(self.monitor)
+
+        return monitor_value
+
+    def _is_improvement(self, monitor_value, reference_value):
+        return self.monitor_op(monitor_value - self.min_delta, reference_value)
+
+class ModelCheckpoint(Callback):
+    """
+    ModelCheckpoint
+    """
+    def __init__(self,
+                 filepath = './',
+                 every_n_epoch = -1,
+                 every_n_iteration = -1):
+        super(ModelCheckpoint).__init__()
+        if every_n_epoch != -1:
+            assert every_n_iteration == -1
+        self.every_n_epoch = every_n_epoch
+        if every_n_iteration != -1:
+            assert every_n_epoch == -1
+        self.every_n_iteration = every_n_iteration
+
+        self.save_path_prefix = filepath
+        self.n_iteration = 0
+
+    def on_epoch_end(self, epochs, logs = None):
+        if self.every_n_epoch >= 1 and epochs % self.every_n_epoch == 0:
+            self._save_model()
+
+    def on_batch_end(self, batch, logs = None):
+        if self.every_n_iteration >=1 and self.n_iteration % self.every_n_iteration == 0:
+            self._save_model()
+        self.n_iteration += 1
+
+    def _save_model(self):
+        self.trainercontrol.should_save = True
 
