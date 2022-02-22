31a78a8a52712d851699dbe224e8add9ee6ea85e
ChengZi <chen.zhang@zilliz.com>
2022-02-22 14:03:50 +0800

    modify callback ut. (#661)
    Signed-off-by: zhang chen <chen.zhang@zilliz.com>

--- tests/unittests/trainer/test_callback.py
+++ tests/unittests/trainer/test_callback.py
@@ -14,33 +14,233 @@
 # limitations under the License.
 
 import unittest
-import torchvision
+from typing import Dict, Tuple
+from tqdm import tqdm
+from towhee.trainer.callback import Callback, CallbackList, TensorBoardCallBack, PrintCallBack, ProgressBarCallBack
 
-from torch import optim
 
+class TestCallback(unittest.TestCase):
+    """
+    TestCallback
+    """
+    logs = {
+        'global_step': 1,
+        'step_loss': 1,
+        'epoch_loss': 1,
+        'epoch_metric': 1,
+        'eval_global_step': 1,
+        'eval_step_loss': 1,
+        'eval_epoch_loss': 1,
+        'eval_epoch_metric': 1,
+        'lr': 1,
+        'epoch': 1
+    }
+    class CustomCallback(Callback):
+        """
+        CustomCallback
+        """
+        def __init__(self):
+            pass
 
-class TrainerCallbackTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.model = torchvision.models.resnet50(pretrained=True)
-        self.optimizer = optim.SGD(self.model.parameters(), lr=0.01, momentum=0.9)
-        self.lr_scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=3, gamma=0.1)
+        def on_batch_begin(self, batch: Tuple, logs: Dict) -> None:
+            logs['on_batch_begin'] = logs['on_batch_begin'] + 1
 
-    def check_callbacks_equality(self, cbs1, cbs2):
-        self.assertEqual(len(cbs1), len(cbs2))
+        def on_batch_end(self, batch: Tuple, logs: Dict) -> None:
+            logs['on_batch_end'] = logs['on_batch_end'] + 1
 
-        # Order doesn't matter
-        cbs1 = list(sorted(cbs1, key=lambda cb: cb.__name__ if isinstance(cb, type) else cb.__class__.__name__))
-        cbs2 = list(sorted(cbs2, key=lambda cb: cb.__name__ if isinstance(cb, type) else cb.__class__.__name__))
-        for cb1, cb2 in zip(cbs1, cbs2):
-            if isinstance(cb1, type) and isinstance(cb2, type):
-                self.assertEqual(cb1, cb2)
-            elif isinstance(cb1, type) and not isinstance(cb2, type):
-                self.assertEqual(cb1, cb2.__class__)
-            elif not isinstance(cb1, type) and isinstance(cb2, type):
-                self.assertEqual(cb1.__class__, cb2)
-            else:
-                self.assertEqual(cb1, cb2)
+        def on_epoch_begin(self, epochs: int, logs: Dict) -> None:
+            logs['on_epoch_begin'] = logs['on_epoch_begin'] + 1
 
+        def on_epoch_end(self, epochs: int, logs: Dict) -> None:
+            logs['on_epoch_end'] = logs['on_epoch_end'] + 1
+
+        def on_train_begin(self, logs: Dict) -> None:
+            logs['on_train_begin'] = logs['on_train_begin'] + 1
+
+        def on_train_end(self, logs: Dict) -> None:
+            logs['on_train_end'] = logs['on_train_end'] + 1
+
+        def on_train_batch_begin(self, batch: Tuple, logs: Dict) -> None:
+            logs['on_train_batch_begin'] = logs['on_train_batch_begin'] + 1
+
+        def on_train_batch_end(self, batch: Tuple, logs: Dict) -> None:
+            logs['on_train_batch_end'] = logs['on_train_batch_end'] + 1
+
+        def on_eval_batch_begin(self, batch: Tuple, logs: Dict) -> None:
+            logs['on_eval_batch_begin'] = logs['on_eval_batch_begin'] + 1
+
+        def on_eval_batch_end(self, batch: Tuple, logs: Dict) -> None:
+            logs['on_eval_batch_end'] = logs['on_eval_batch_end'] + 1
+
+        def on_eval_begin(self, logs: Dict) -> Dict:
+            logs['on_eval_begin'] = logs['on_eval_begin'] + 1
+
+        def on_eval_end(self, logs: Dict) -> Dict:
+            logs['on_eval_end'] = logs['on_eval_end'] + 1
+
+    def _init_log(self) -> Dict:
+        logs = {}
+        logs['on_batch_begin'] = 0
+        logs['on_batch_end'] = 0
+        logs['on_epoch_begin'] = 0
+        logs['on_epoch_end'] = 0
+        logs['on_train_batch_begin'] = 0
+        logs['on_train_batch_end'] = 0
+        logs['on_train_begin'] = 0
+        logs['on_train_end'] = 0
+        logs['on_eval_batch_begin'] = 0
+        logs['on_eval_batch_end'] = 0
+        logs['on_eval_begin'] = 0
+        logs['on_eval_end'] = 0
+        return logs
+
+    def test_callback(self) -> None:
+        train_log = self._init_log()
+        eval_log = self._init_log()
+        callback = TestCallback.CustomCallback()
+
+        #simulate the train loop.
+        train_epoch_nums = 3
+        train_batch_nums = 5
+        callback.on_train_begin(train_log)
+        for train_epoch in range(train_epoch_nums):
+            callback.on_epoch_begin(train_epoch, train_log)
+            for _ in range(train_batch_nums):
+                mock_batch = (None, None)
+                callback.on_train_batch_begin(mock_batch, train_log)
+                callback.on_train_batch_end(mock_batch, train_log)
+            callback.on_epoch_end(train_epoch, train_log)
+        callback.on_train_end(train_log)
+
+        def _assert_train_attrs(attr, gt):
+            self.assertEqual(train_log[attr], gt)
+
+        _assert_train_attrs('on_batch_begin', 0)
+        _assert_train_attrs('on_batch_end', 0)
+        _assert_train_attrs('on_epoch_begin', train_epoch_nums)
+        _assert_train_attrs('on_epoch_end', train_epoch_nums)
+        _assert_train_attrs('on_train_batch_begin', train_epoch_nums * train_batch_nums)
+        _assert_train_attrs('on_train_batch_end', train_epoch_nums * train_batch_nums)
+        _assert_train_attrs('on_train_begin', 1)
+        _assert_train_attrs('on_train_end', 1)
+
+        #simulate the eval loop.
+        eval_epoch_nums = 5
+        eval_batch_nums = 10
+        eval_log = self._init_log()
+        callback.on_eval_begin(eval_log)
+        for eval_epoch in range(eval_epoch_nums):
+            callback.on_epoch_begin(eval_epoch, eval_log)
+            for _ in range(eval_batch_nums):
+                mock_batch = (None, None)
+                callback.on_eval_batch_begin(mock_batch, eval_log)
+                callback.on_eval_batch_end(mock_batch, eval_log)
+            callback.on_epoch_end(eval_epoch, eval_log)
+        callback.on_eval_end(eval_log)
+
+        def _assert_eval_attrs(attr, gt):
+            self.assertEqual(eval_log[attr], gt)
+
+        _assert_eval_attrs('on_batch_begin', 0)
+        _assert_eval_attrs('on_batch_end', 0)
+        _assert_eval_attrs('on_epoch_begin', eval_epoch_nums)
+        _assert_eval_attrs('on_epoch_end', eval_epoch_nums)
+        _assert_eval_attrs('on_eval_batch_begin', eval_epoch_nums * eval_batch_nums)
+        _assert_eval_attrs('on_eval_batch_end', eval_epoch_nums * eval_batch_nums)
+        _assert_eval_attrs('on_eval_begin', 1)
+        _assert_eval_attrs('on_eval_end', 1)
+
+    def test_callbackslist(self) -> None:
+        callbacklist = CallbackList([TestCallback.CustomCallback(), TestCallback.CustomCallback()])
+
+        cb = TestCallback.CustomCallback()
+        callbacklist.add_callback(cb)
+        print(callbacklist)
+        self.assertEqual(len(callbacklist), 3)
+        callbacklist.pop_callback(cb)
+        self.assertEqual(len(callbacklist), 2)
+
+        train_log = self._init_log()
+        eval_log = self._init_log()
+
+        train_epoch_nums = 3
+        train_batch_nums = 5
+        #simulate the train loop.
+        callbacklist.on_train_begin(train_log)
+        for train_epoch in range(train_epoch_nums):
+            callbacklist.on_epoch_begin(train_epoch, train_log)
+            for _ in range(train_batch_nums):
+                mock_batch = (None, None)
+                callbacklist.on_train_batch_begin(mock_batch, train_log)
+                callbacklist.on_train_batch_end(mock_batch, train_log)
+            callbacklist.on_epoch_end(train_epoch, train_log)
+        callbacklist.on_train_end(train_log)
+
+        def _assert_train_attrs(attr, gt):
+            self.assertEqual(train_log[attr], gt)
+
+        num_callbacks = len(callbacklist)
+        _assert_train_attrs('on_batch_begin', 0 * num_callbacks)
+        _assert_train_attrs('on_batch_end', 0 * num_callbacks)
+        _assert_train_attrs('on_epoch_begin', train_epoch_nums * num_callbacks)
+        _assert_train_attrs('on_epoch_end', train_epoch_nums * num_callbacks)
+        _assert_train_attrs('on_train_batch_begin', train_epoch_nums * train_batch_nums * num_callbacks)
+        _assert_train_attrs('on_train_batch_end', train_epoch_nums * train_batch_nums * num_callbacks)
+        _assert_train_attrs('on_train_begin', 1 * num_callbacks)
+        _assert_train_attrs('on_train_end', 1 * num_callbacks)
+
+        #simulate the eval loop.
+        eval_epoch_nums = 5
+        eval_batch_nums = 10
+        callbacklist.on_eval_begin(eval_log)
+        for eval_epoch in range(eval_epoch_nums):
+            callbacklist.on_epoch_begin(eval_epoch, eval_log)
+            for _ in range(eval_batch_nums):
+                mock_batch = (None, None)
+                callbacklist.on_eval_batch_begin(mock_batch, eval_log)
+                callbacklist.on_eval_batch_end(mock_batch, eval_log)
+            callbacklist.on_epoch_end(eval_epoch, eval_log)
+        callbacklist.on_eval_end(eval_log)
+
+        def _assert_eval_attrs(attr, gt):
+            self.assertEqual(eval_log[attr], gt)
+
+        _assert_eval_attrs('on_batch_begin', 0 * num_callbacks)
+        _assert_eval_attrs('on_batch_end', 0 * num_callbacks)
+        _assert_eval_attrs('on_epoch_begin', eval_epoch_nums * num_callbacks)
+        _assert_eval_attrs('on_epoch_end', eval_epoch_nums * num_callbacks)
+        _assert_eval_attrs('on_eval_batch_begin', eval_epoch_nums * eval_batch_nums * num_callbacks)
+        _assert_eval_attrs('on_eval_batch_end', eval_epoch_nums * eval_batch_nums * num_callbacks)
+        _assert_eval_attrs('on_eval_begin', 1 * num_callbacks)
+        _assert_eval_attrs('on_eval_end', 1 * num_callbacks)
+
+    def test_tensorboard(self):
+        class MockSW:
+            def __init__(self, log_dir, comment=''):
+                self.log_dir = log_dir
+                self.comment = comment
+                self.sw_list = []
+            def __call__(self, *args, **kwargs):
+                return self
+            def add_scalar(self, *args):
+                self.sw_list.append(args)
+
+        mock_sw = MockSW(log_dir='runs')
+
+        tensorboard_callback = TensorBoardCallBack(summary_writer_constructor=mock_sw)
+        tensorboard_callback.on_train_batch_end(batch=(1,), logs=self.logs)
+        tensorboard_callback.on_eval_batch_end(batch=(1,), logs=self.logs)
+
+    def test_print(self):
+        print_callback = PrintCallBack(total_epoch_num=10, step_frequency=1)
+        print_callback.on_train_batch_end(batch=(1,), logs=self.logs)
+        print_callback.on_eval_batch_end(batch=(1,), logs=self.logs)
+
+    def test_progressbar(self):
+        progressbar_callback = ProgressBarCallBack(total_epoch_num=10, train_dataloader=tqdm(range(2)))
+        progressbar_callback.on_train_batch_end(batch=(1,), logs=self.logs)
+        progressbar_callback.on_epoch_begin(epochs=1, logs=self.logs)
+        progressbar_callback.on_eval_batch_end(batch=(1,), logs=self.logs)
 
 if __name__ == '__main__':
     unittest.main(verbosity=1)
