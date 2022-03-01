ee503638d0e5a7b6da95838a64c01852dd06f046
Jael Gu <mengjia.gu@zilliz.com>
2022-03-01 14:45:53 +0800

    Optimize and add docstring to layer_freezer (#720)
    Signed-off-by: Jael Gu <mengjia.gu@zilliz.com>

--- tests/unittests/trainer/test_freezer.py
+++ tests/unittests/trainer/test_freezer.py
@@ -27,29 +27,29 @@ class TestFreezer(unittest.TestCase):
     def test_freezer_status(self):
         res1 = self.freezer.status(-1)
         res2 = self.freezer.status('fc')
-        self.assertEqual(res1, res2, ['unfreezed' for _ in range(len(res1))])
+        self.assertEqual(res1, res2, ['unfrozen' for _ in range(len(res1))])
 
     def test_freezer_by_name(self):
         self.freezer.by_names(['conv1', 'fc'])
         res1 = self.freezer.status('fc')
         res2 = self.freezer.status('conv1')
-        self.assertEqual(res1 + res2, ['freezed' for _ in range(len(res1 + res2))])
+        self.assertEqual(res1 + res2, ['frozen' for _ in range(len(res1 + res2))])
 
         self.freezer.by_names(['conv1', 'fc'], freeze=False)
         res1 = self.freezer.status('fc')
         res2 = self.freezer.status('conv1')
-        self.assertEqual(res1 + res2, ['unfreezed' for _ in range(len(res1 + res2))])
+        self.assertEqual(res1 + res2, ['unfrozen' for _ in range(len(res1 + res2))])
 
     def test_freezer_by_idx(self):
         self.freezer.by_idx([0, -1])
         res1 = self.freezer.status(0)
         res2 = self.freezer.status(-1)
-        self.assertEqual(res1 + res2, ['freezed' for _ in range(len(res1 + res2))])
+        self.assertEqual(res1 + res2, ['frozen' for _ in range(len(res1 + res2))])
 
         self.freezer.by_idx([0, -1], freeze=False)
         res1 = self.freezer.status(0)
         res2 = self.freezer.status(-1)
-        self.assertEqual(res1 + res2, ['unfreezed' for _ in range(len(res1 + res2))])
+        self.assertEqual(res1 + res2, ['unfrozen' for _ in range(len(res1 + res2))])
 
     def test_freezer_all(self):
         self.freezer.set_all()
@@ -58,7 +58,7 @@ class TestFreezer(unittest.TestCase):
             r = self.freezer.status(i)
             for x in r:
                 res.append(x)
-        self.assertEqual(res, ['freezed' for _ in range(len(res))])
+        self.assertEqual(res, ['frozen' for _ in range(len(res))])
 
         self.freezer.set_all(freeze=False)
         res = []
@@ -66,11 +66,11 @@ class TestFreezer(unittest.TestCase):
             r = self.freezer.status(i)
             for x in r:
                 res.append(x)
-        self.assertEqual(res, ['unfreezed' for _ in range(len(res))])
+        self.assertEqual(res, ['unfrozen' for _ in range(len(res))])
 
     def test_freezer_slice(self):
         self.freezer.set_slice(1)
-        res = self.freezer.show_freezed_layers()
+        res = self.freezer.show_frozen_layers()
         self.assertEqual(res, ['conv1'])
 
 
