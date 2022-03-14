b108f1200a9e08539540cde94c85cf3a9cd392b1
Reiase <jie.hou@zilliz.com>
2022-03-14 17:36:00 +0800

    support grab index parameters in call holder (#783)
    Signed-off-by: jie.hou <jie.hou@zilliz.com>

--- towhee/functional/mixins/dispatcher.py
+++ towhee/functional/mixins/dispatcher.py
@@ -12,16 +12,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Any
+
 from towhee.engine.operator_loader import OperatorLoader
+
+
+class _OperatorWrapper:
+
+    def __init__(self, op, index):
+        self._op = op
+        self._index = index
+        print(index)
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self._op(*args, **kwds)
 
 
 class DispatcherMixin:
     """
     Mixin for call dispatcher for data collection
+
+    >>> from towhee import register
+    >>> from towhee import ops
+    >>> from towhee.functional import DataCollection
+    >>> @register('0.1', name='add_1')
+    ... def add_1(x):
+    ...     return x+1
+
+    >>> dc = DataCollection.range(5).stream()
+    >>> dc.add_1['a','b','c']()
     """
-    def resolve(self, call_mapping, path, *arg, **kws):
+
+    def resolve(self, call_mapping, path, index, *arg, **kws):
+        _ = index
         if path in call_mapping:
             return call_mapping[path](*arg, **kws)
         else:
             loader = OperatorLoader()
-            return loader.load_operator(path.replace('.', '/').replace('_', '-'), kws, tag='main')
+            op = loader.load_operator(path.replace('.', '/').replace('_', '-'),
+                                      kws,
+                                      tag='main')
+            return _OperatorWrapper(op, index)
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod(verbose=False)
+    # from towhee import register
+    # from towhee import ops
+    # from towhee.functional import DataCollection
+    # @register('0.1', name='add_1')
+    # def add_1(x):
+    #     return x+1
+
+    # dc = DataCollection.range(5).stream()
+    # # dc.map(ops.add_1[('a','b','c')]())
+    # print(ops.add_1)
+    # print(ops.add_1['a']())
+    # dc.add_1['a','b',('c', 'd')]()
+    # # import ipdb; ipdb.set_trace()
