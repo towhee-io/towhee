8d5ffaee85047c90ba96870cd33825f9e91982c5
shiyu22 <shiyu.chen@zilliz.com>
2022-03-17 17:11:30 +0800

    Mixin API for DC
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>

---
+++ towhee/functional/mixins/dataset.py
@@ -0,0 +1,76 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class DatasetMixin:
+    """
+        Mixin for dealing with dataset
+    """
+    def from_glob(self):
+        # data_source.py already exists
+        pass
+
+    def from_zip(self):
+        # data_source.py already exists
+        pass
+
+    def from_camera(self):
+        # data_source.py already exists
+        pass
+
+    def image_decode(self):
+        pass
+
+    def image_encode(self):
+        pass
+
+    def audio_decode(self):
+        pass
+
+    def audio_encode(self):
+        pass
+
+    def video_decode(self):
+        pass
+
+    def video_encode(self):
+        pass
+
+    def from_json(self):
+        pass
+
+    def from_csv(self):
+        pass
+
+    def random_sample(self):
+        # core API already exists
+        pass
+
+    def filter_data(self):
+        # core API already exists
+        pass
+
+    def split_train_test(self):
+        pass
+
+    def save_json(self):
+        pass
+
+    def save_image(self):
+        pass
+
+    def save_csv(self):
+        pass
+
+    def get_info(self):
+        pass
