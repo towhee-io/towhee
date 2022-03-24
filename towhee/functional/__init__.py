d7c8476f935c61b42fb46e763b7b28cc7095099e
Kaiyuan Hu <kaiyuan.hu@zilliz.com>
2022-03-24 18:01:31 +0800

    Allow import Entity from towhee and towhee.functional
    Signed-off-by: Kaiyuan Hu <kaiyuan.hu@zilliz.com>

--- towhee/functional/__init__.py
+++ towhee/functional/__init__.py
@@ -12,5 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .data_collection import DataCollection
+from .entity import Entity
 from .option import Option, Some, Empty
 from towhee.hparam import HyperParameter as State
