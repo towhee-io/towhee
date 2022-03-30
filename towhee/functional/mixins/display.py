c8bbd8fa118e6e1b8e4d89e0cdf72088a1d03fbd
shiyu22 <shiyu.chen@zilliz.com>
2022-03-30 18:59:14 +0800

    Support value_counts and plot in DC
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>

--- towhee/functional/mixins/display.py
+++ towhee/functional/mixins/display.py
@@ -11,20 +11,74 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Iterable
-
+from typing import Iterable, Tuple
 from tabulate import tabulate
+
+from towhee.hparam import param_scope
+
+
+def _value_counts_callback(self):
+    def wrapper(_: str, index: Tuple[str], *arg, **kws):
+        # pylint: disable=import-outside-toplevel
+        # pylint: disable=unused-argument
+        import pandas as pd
+        columns = [[] for _ in index]
+        for x in self:
+            for i in range(len(index)):
+                columns[i].append(getattr(x, index[i]))
+
+        data = {}
+        for i in range(len(index)):
+            data[index[i]] = columns[i]
+        df = pd.DataFrame(data)
+        print(df.value_counts(**kws))
+
+    return wrapper
+
+
+def _plot_callback(self):
+    def wrapper(_: str, index: Tuple[str], *arg, **kws):
+        # pylint: disable=import-outside-toplevel
+        # pylint: disable=unused-argument
+        import pandas as pd
+        columns = [[] for _ in index]
+        for x in self:
+            for i in range(len(index)):
+                columns[i].append(getattr(x, index[i]))
+
+        data = {}
+        for i in range(len(index)):
+            data[index[i]] = columns[i]
+        df = pd.DataFrame(data)
+        if 'kind' not in kws:
+            kws.update(kind='hist')
+        df.plot(**kws)
+
+    return wrapper
 
 
 class DisplayMixin:
     """
-        Mixin for display data
+    Mixin for display data
+
+    Example:
+
+    >>> from towhee import DataCollection
+    >>> from towhee import Entity
+    >>> dc = DataCollection([Entity(a=i, b=i) for i in range(2)])
+    >>> dc.value_counts['a'](normalize=True)
+    a
+    0    0.5
+    1    0.5
+    dtype: float64
+    >>> dc.plot['a']()
     """
+    def __init__(self):
+        self.value_counts = param_scope().callholder(_value_counts_callback(self))
+        self.plot = param_scope().callholder(_plot_callback(self))
+
     def as_str(self):
         return self.factory(map(str, self._iterable))
-
-    def plot(self):
-        pass
 
     def head(self, num: int = 5, header: Iterable[str] = None, numalign: str = 'center', stralign: str = 'center', tablefmt: str = 'plain'):
         """
@@ -83,3 +137,8 @@ class DisplayMixin:
             to_display.append(i.to_dict().values())
 
         print(tabulate(to_display, headers=header, tablefmt=tablefmt, numalign=numalign, stralign=stralign))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=False)
