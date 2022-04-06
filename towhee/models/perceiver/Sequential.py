657826a77fe6a364d4d239b08eef04c39b8f5c37
Junxen <Junxen@users.noreply.github.com>
2022-04-06 10:11:19 +0800

    add create cross attention function for perceiver

---
+++ towhee/models/perceiver/Sequential.py
@@ -0,0 +1,17 @@
+# original code from https://github.com/krasserm/perceiver-io
+
+
+from torch import nn
+
+
+class Sequential(nn.Sequential):
+    """
+    Sequential module for Perceiver https://arxiv.org/pdf/2103.03206.pdf.
+    """
+    def forward(self, *inputs):
+        for module in self:
+            if type(inputs) == tuple:
+                inputs = module(*inputs)
+            else:
+                inputs = module(inputs)
+        return inputs
