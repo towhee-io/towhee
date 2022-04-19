0dd5f8f64890afdb14f8bf078bb81d398cc7c798
Jenny Li <jing.li@zilliz.com>
2022-04-19 20:44:43 +0800

    Add debug code
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- towhee/utils/git_utils.py
+++ towhee/utils/git_utils.py
@@ -107,7 +107,9 @@ class GitUtils:
             ) from e
 
         try:
-            subprocess.check_call(['git', 'clone', '-b', tag, url, local_repo_path])
+            output = subprocess.check_call(['git', 'clone', '-b', tag, url, local_repo_path],stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            print("Exception on process, rc=", e.returncode, "output=", e.output)
         except FileNotFoundError as e:
             engine_log.warning(
                 '\'git\' not found, execute download instead of clone. ' \
