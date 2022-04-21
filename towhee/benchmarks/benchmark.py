6c6ca87952a8f904c9abeedde1c3008f348fcaaf
Filip Haltmayer <filip.haltmayer@zilliz.com>
2022-04-20 17:23:40 -0700

    cleaned up a little
    Signed-off-by: Filip Haltmayer <filip.haltmayer@zilliz.com>

--- towhee/benchmarks/benchmark.py
+++ towhee/benchmarks/benchmark.py
@@ -10,19 +10,23 @@ from pathlib import Path #pylint: disable=wrong-import-position
 from towhee.hub.file_manager import FileManagerConfig, FileManager #pylint: disable=wrong-import-position
 from towhee.benchmarks import runner_engine, dc_engine #pylint: disable=wrong-import-position
 
+def startup():
+    fmc = FileManagerConfig()
+    cache_path = Path(__file__).parent.resolve()
+    new_cache_path = cache_path / 'cache'
+    pipeline_cache = cache_path / 'yamls'
+    fmc.update_default_cache(new_cache_path)
+    pipelines = list(pipeline_cache.rglob('*.yaml'))
+    fmc.cache_local_pipeline(pipelines)
+    FileManager(fmc)
 
-fmc = FileManagerConfig()
-CACHE_PATH = Path(__file__).parent.resolve()
-new_cache_path = CACHE_PATH / 'cache'
-pipeline_cache = CACHE_PATH / 'yamls'
-fmc.update_default_cache(new_cache_path)
-pipelines = list(pipeline_cache.rglob('*.yaml'))
-fmc.cache_local_pipeline(pipelines)
-FileManager(fmc)
+def three_dots(loops, size, threads):
+    x = runner_engine.three_dots(loops, size, threads)
+    y = dc_engine.three_dots(loops, size, threads*3) # Runner is threads per task, dc is threads per pipeline
+    print('Three Dots:')
+    print('Runner:', x)
+    print('DC', y)
 
-x = runner_engine.three_dots(1000, 500, 6)
-print(x)
-y = dc_engine.three_dots(1000, 500, 18)
-print(y)
-
+startup()
+three_dots(100, 1000, 2)
 
