5db9bfb06d4bc6c39308c233b1103f2a8e4a4007
Junchen He <hovmey@hotmail.com>
2022-05-06 11:17:49 +0800

    add lrp for patch embed 2d layer (#1191)
    Co-authored-by: Junxen <Junxen@users.noreply.github.com>

--- tests/unittests/models/layers/test_patch_embed2d.py
+++ tests/unittests/models/layers/test_patch_embed2d.py
@@ -30,3 +30,13 @@ class PatchEmbed2DTest(unittest.TestCase):
         out2 = model(fake_video)
         self.assertTrue(out1.shape == (1, 196, 768))
         self.assertTrue(out2.shape == (5, 196, 768))
+
+    def test_patch_embed2d_with_lrp(self):
+        test_shape1 = (1, 3, 224, 224)
+        fake_img = torch.rand(test_shape1)
+        kwargs = {'alpha': 1}
+        model = PatchEmbed2D()
+        out1 = model(fake_img)
+        # torch.Size([1, 3, 224, 224])
+        out2 = model.relprop(out1, **kwargs)
+        self.assertTrue(out2.shape == torch.Size([1, 3, 224, 224]))
