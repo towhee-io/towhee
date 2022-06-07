688690eb7d71055de52e1766c9a05ad02bd45d4d
gexy5 <xinyu.ge@zilliz.com>
2022-06-07 10:25:05 +0800

    simplify the tsm model test
    Signed-off-by: gexy5 <xinyu.ge@zilliz.com>

--- tests/unittests/models/tsm/test_tsm.py
+++ tests/unittests/models/tsm/test_tsm.py
@@ -18,86 +18,109 @@ from towhee.models.tsm.tsm import TSN
 
 class TSMTest(unittest.TestCase):
     def test_tsm_resnet_rgb(self):
-        num_class = 256
+        num_class = 2
         num_segments = 1
         modality = 'RGB'
         is_shift = True
         non_local = True
-        input_tensor=torch.randn(10,3,224,224)
-        model = TSN(num_class = num_class, num_segments = num_segments, modality = modality, is_shift = is_shift, non_local = non_local)
+        pretrain = False
+        input_tensor=torch.randn(2,3,32,32)
+        model = TSN(num_class = num_class,
+                    num_segments = num_segments,
+                    modality = modality,
+                    is_shift = is_shift,
+                    non_local = non_local,
+                    pretrain = pretrain)
         out = model(input_tensor)
-        self.assertTrue(out.shape == torch.Size([10,256]))
+        self.assertTrue(out.shape == torch.Size([2,2]))
 
     def test_tsm_resnet_flow(self):
-        num_class = 256
+        num_class = 2
         num_segments = 1
         modality = 'Flow'
         is_shift = True
         non_local = True
-        input_tensor=torch.randn(10,3,224,224)
-        model = TSN(num_class = num_class, num_segments = num_segments, modality = modality, is_shift = is_shift, non_local = non_local)
+        pretrain = False
+        input_tensor=torch.randn(2,10,32,32)
+        model = TSN(num_class = num_class,
+                    num_segments = num_segments,
+                    modality = modality,
+                    is_shift = is_shift,
+                    non_local = non_local,
+                    pretrain = pretrain)
         out = model(input_tensor)
-        self.assertTrue(out.shape == torch.Size([3,256]))
+        self.assertTrue(out.shape == torch.Size([2,2]))
 
     def test_tsm_resnet_diff(self):
-        num_class = 256
+        num_class = 2
         num_segments = 3
         modality = 'RGBDiff'
         is_shift = True
         non_local = True
-        input_tensor=torch.randn(54,3,224,224)
-        model = TSN(num_class = num_class, num_segments = num_segments, modality = modality, is_shift = is_shift, non_local = non_local)
+        pretrain = False
+        input_tensor=torch.randn(54,1,32,32)
+        model = TSN(num_class = num_class,
+                    num_segments = num_segments,
+                    modality = modality,
+                    is_shift = is_shift,
+                    non_local = non_local,
+                    pretrain = pretrain)
         out = model(input_tensor)
-        self.assertTrue(out.shape == torch.Size([3,256]))
+        self.assertTrue(out.shape == torch.Size([1,2]))
 
     def test_tsm_mobilenetv2_rgb(self):
-        num_class = 256
+        num_class = 2
         num_segments = 1
         modality = 'RGB'
         base_model = 'mobilenetv2'
         is_shift = True
         non_local = True
-        input_tensor=torch.randn(10,3,224,224)
+        pretrain = False
+        input_tensor=torch.randn(2,3,32,32)
         model = TSN(num_class = num_class,
                     num_segments = num_segments,
                     modality = modality,
                     base_model = base_model,
                     is_shift = is_shift,
-                    non_local = non_local)
+                    non_local = non_local,
+                    pretrain = pretrain)
         out = model(input_tensor)
-        self.assertTrue(out.shape == torch.Size([10,256]))
+        self.assertTrue(out.shape == torch.Size([2,2]))
 
     def test_tsm_mobilenetv2_flow(self):
-        num_class = 256
+        num_class = 2
         num_segments = 1
         modality = 'Flow'
         base_model = 'mobilenetv2'
         is_shift = True
         non_local = True
-        input_tensor=torch.randn(10,3,224,224)
+        pretrain = False
+        input_tensor=torch.randn(2,10,32,32)
         model = TSN(num_class = num_class,
                     num_segments = num_segments,
                     modality = modality,
                     base_model = base_model,
                     is_shift = is_shift,
-                    non_local = non_local)
+                    non_local = non_local,
+                    pretrain = pretrain)
         out = model(input_tensor)
-        self.assertTrue(out.shape == torch.Size([3,256]))
+        self.assertTrue(out.shape == torch.Size([2,2]))
 
     def test_tsm_mobilenetv2_diff(self):
-        num_class = 256
+        num_class = 2
         num_segments = 3
         modality = 'RGBDiff'
         base_model = 'mobilenetv2'
         is_shift = True
         non_local = True
-        input_tensor=torch.randn(54,3,224,224)
+        pretrain = False
+        input_tensor=torch.randn(54,1,32,32)
         model = TSN(num_class = num_class,
                     num_segments = num_segments,
                     modality = modality,
                     base_model = base_model,
                     is_shift = is_shift,
-                    non_local = non_local)
+                    non_local = non_local,
+                    pretrain = pretrain)
         out = model(input_tensor)
-        self.assertTrue(out.shape == torch.Size([3,256]))
-
+        self.assertTrue(out.shape == torch.Size([1,2]))
