601f8f7a041f67d2384585f1349089f88f77f785
xujinling <jinling.xu@zilliz.com>
2022-06-16 18:06:14 +0800

    modify clip to adapt bridgeformer
    Signed-off-by: xujinling <jinling.xu@zilliz.com>

---
+++ towhee/models/bridgeformer/bridge_former.py
@@ -0,0 +1,65 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from towhee.models.clip import CLIP
+from towhee.models.frozen_in_time import FrozenInTime
+from towhee.models.bridgeformer.bridge_former_utils import convert_weights, clip_initialized_model_configs
+from towhee.models.frozen_in_time.frozen_utils import state_dict_data_parallel_fix
+
+
+def create_model(
+        model_name: str = None,
+        pretrained: bool = False,
+        weights_path: str = None,
+        device: str = None,
+        **kwargs
+):
+    """
+    create the bridgeformer model
+    model_name: if use the clip_initialized_model then set "clip_initialized_model"
+    """
+    if device is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+    if pretrained and weights_path is None:
+        raise AssertionError("if pretrained is true, weights_path needs to be specified")
+    if model_name == "clip_initialized_model":
+
+        if pretrained:
+            configs = clip_initialized_model_configs()
+            model = CLIP(**configs)
+            checkpoint = torch.load(weights_path, map_location=device)
+            state_dict = checkpoint["state_dict"]
+            state_dict = state_dict_data_parallel_fix(state_dict, model.state_dict())
+            for key in ["input_resolution", "context_length", "vocab_size"]:
+                if key in state_dict:
+                    del state_dict[key]
+            convert_weights(model)
+            model.load_state_dict(state_dict, strict=True)
+            if device == "cpu":
+                # CPU training, does not support half-precision numbers
+                model = model.float()
+        else:
+            model = CLIP(
+                is_bridge_former=True, **kwargs
+            )
+    else:
+        model = FrozenInTime(weights_path=weights_path,
+                             is_pretrained=pretrained,
+                             attention_style='bridge_former',
+                             device=device,
+                             **kwargs)
+
+    model.eval()
+    model.to(device)
+    return model
+
---
+++ towhee/models/bridgeformer/bridge_former_utils.py
@@ -0,0 +1,50 @@
+# Original pytorch implementation by:
+# 'Bridging Video-text Retrieval with Multiple Choice Questions'
+#       - https://arxiv.org/pdf/2201.04850.pdf
+# Original code by / Copyright 2022, Yuying Ge.
+# Modifications & additions by / Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from torch import nn
+
+
+def clip_initialized_model_configs():
+    return dict(
+        embed_dim=512,
+        image_resolution=224, vision_layers=12, vision_width=768, vision_patch_size=32,
+        context_length=77, vocab_size=49408, transformer_width=512, transformer_heads=8, transformer_layers=12,
+        is_bridge_former=True
+    )
+
+def convert_weights(model: nn.Module):
+    """Convert applicable model parameters to fp16"""
+
+    def _convert_weights_to_fp16(l):
+        if isinstance(l, (nn.Conv1d, nn.Conv2d, nn.Linear)):
+            l.weight.data = l.weight.data.half()
+            if l.bias is not None:
+                l.bias.data = l.bias.data.half()
+
+        if isinstance(l, nn.MultiheadAttention):
+            for attr in [*[f"{s}_proj_weight" for s in ["in", "q", "k", "v"]], "in_proj_bias", "bias_k", "bias_v"]:
+                tensor = getattr(l, attr)
+                if tensor is not None:
+                    tensor.data = tensor.data.half()
+
+        for name in ["text_projection", "proj"]:
+            if hasattr(l, name):
+                attr = getattr(l, name)
+                if attr is not None:
+                    attr.data = attr.data.half()
+
+    model.apply(_convert_weights_to_fp16)
