bd840face8a15474b2b338b95fed3932c379c38a
GuoRentong <rentong.guo@zilliz.com>
2022-06-17 20:11:43 +0800

    Add _get_triton_schema in TritonModelConfigBuilder
    Signed-off-by: GuoRentong <rentong.guo@zilliz.com>

--- serve/triton/python_model_builder.py
+++ serve/triton/python_model_builder.py
@@ -242,7 +242,119 @@ class PickledCallablePyModelBuilder(PyModelBuilder):
 
 
 class OpPyModelBuilder:
-    pass
+    """
+    Build python model from operator
+    """
+
+    def __init__(
+        self,
+        op_name: str,
+        input_annotations: List[Tuple[Any, Tuple]],
+        output_annotations: List[Tuple[Any, Tuple]]
+    ):
+        self.op_name = op_name
+        self.input_annotations = input_annotations
+        self.output_annotations = output_annotations
+
+    def gen_imports(self):
+        lines = []
+        lines.append('import towhee')
+        lines.append('import triton_python_backend_utils as pb_utils')
+
+        return fmt.add_line_separator(lines)
+
+    def gen_initialize(self):
+        lines = []
+        lines.append('def initialize(self, args):')
+        lines.append('')
+        lines.append('# load module')
+        lines.append('spec = importlib.util.spec_from_file_location(\'' + self.module_name + '\', \'' + self.python_file_path + '\')')
+        lines.append('module = importlib.util.module_from_spec(spec)')
+        lines.append('sys.modules[\'' + self.module_name + '\'] = module')
+        lines.append('spec.loader.exec_module(module)')
+        lines.append('')
+        lines.append('# create callable object')
+        lines.append('callable_cls = ' + self.module_name + '.' + self.callable_name)
+        lines.append('with open(\'' + self.pickle_file_path + '\', \'rb\') as f:')
+        lines.append(fmt.intend('self.callable_obj = pickle.load(f)'))
+
+        lines = lines[:1] + fmt.intend(lines[1:])
+        return fmt.add_line_separator(lines)
+
+    def gen_execute(self):
+        lines = []
+        lines.append('def execute(self, requests):')
+        lines.append('')
+        lines.append('responses = []')
+        lines.append('')
+
+        taskloop = []
+        taskloop.append('for request in requests:')
+        taskloop.append('# get input tensors from request')
+
+        input_type_info = tygen.get_type_info(self.input_annotations)
+        input_arg_init_code = tygen.get_init_code(self.input_annotations)
+
+        tr_idx = 0
+        callable_input_args = []
+
+        for arg_idx, type_info in enumerate(input_type_info):
+            arg = 'arg' + str(arg_idx)
+            callable_input_args.append(arg)
+            num_attrs = len(type_info.attr_info)
+
+            tr_vars = ['in' + str(tr_idx + i) for i in range(num_attrs)]
+            for tr in tr_vars:
+                taskloop.append(tr + ' = pb_utils.get_input_tensor_by_name(request, \'INPUT' + str(tr_idx) + '\')')
+                tr_idx = tr_idx + 1
+
+            taskloop.append('')
+            taskloop.append('# create input args from tensors')
+
+            l = self._from_tensor_to_obj(type_info, input_arg_init_code[arg_idx], arg, tr_vars)
+            taskloop = taskloop + l
+
+        taskloop.append('')
+        taskloop.append('# call callable object')
+        callable_results = ['result' + str(i) for i in range(len(self.output_annotations))]
+        taskloop.append(', '.join(callable_results) + ' = self.callable_obj(' + ' ,'.join(callable_input_args) + ')')
+
+        taskloop.append('')
+        taskloop.append('# convert results to tensors')
+
+        output_type_info = tygen.get_type_info(self.output_annotations)
+
+        tr_idx = 0
+        tr_out = []
+
+        for result_idx, type_info in enumerate(output_type_info):
+            num_attrs = len(type_info.attr_info)
+            tr_vars = ['out' + str(tr_idx + i) for i in range(num_attrs)]
+            tr_out = tr_out + tr_vars
+            outputs = ['OUTPUT' + str(tr_idx + i) for i in range(num_attrs)]
+            l = self._from_obj_to_tensor(type_info, callable_results[result_idx], tr_vars, outputs)
+            taskloop = taskloop + l
+
+        taskloop.append('')
+        taskloop.append('# organize response')
+        taskloop.append('response = pb_utils.InferenceResponse(output_tensors=[' + ', '.join(tr_out) + '])')
+        taskloop.append('responses.append(response)')
+
+        taskloop = taskloop[:1] + fmt.intend(taskloop[1:])
+        lines = lines + taskloop
+
+        lines.append('')
+        lines.append('return responses')
+
+        lines = lines[:1] + fmt.intend(lines[1:])
+
+        return fmt.add_line_separator(lines)
+
+    def gen_finalize(self):
+        lines = []
+        lines.append('def finalize(self):')
+        lines.append(fmt.intend('pass'))
+        return fmt.add_line_separator(lines)
 
 
 def gen_model_from_pickled_callable(
---
+++ serve/triton/triton_config_builder.py
@@ -0,0 +1,50 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict, List, Tuple, Any
+
+import serve.triton.type_gen as tygen
+import serve.triton.format_utils as fmt
+
+
+class TritonModelConfigBuilder:
+    """
+    This class is used to generate a triton model config file.
+    """
+
+    def __init__(self, model_spec: Dict):
+        self.model_spec = model_spec
+
+    @staticmethod
+    def _get_triton_schema(schema: List[Tuple[Any, Tuple]], var_prefix):
+
+        type_info_list = tygen.get_type_info(schema)
+        attr_info_list = [attr_info for type_info in type_info_list for attr_info in type_info.attr_info]
+
+        schema = {}
+        for i, attr_info in enumerate(attr_info_list):
+            var = var_prefix + str(i)
+            dtype = attr_info.triton_dtype
+            shape = list(attr_info.shape)
+            schema[var] = (dtype, shape)
+
+        return schema
+
+    @staticmethod
+    def get_input_schema(schema: List[Tuple[Any, Tuple]]):
+        return TritonModelConfigBuilder._get_triton_schema(schema, 'INPUT')
+
+    @staticmethod
+    def get_output_schema(schema: List[Tuple[Any, Tuple]]):
+        return TritonModelConfigBuilder._get_triton_schema(schema, 'OUTPUT')
