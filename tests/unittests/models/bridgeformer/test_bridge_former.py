a18c3dda2cb2e630ae76f86e694dfb981fb3ddcf
jinlingxu06 <106302799+jinlingxu06@users.noreply.github.com>
2022-06-17 14:56:10 +0800

    modify clip to adapt bridgeformer (#1408)
    Signed-off-by: xujinling <jinling.xu@zilliz.com>

--- tests/unittests/models/bridgeformer/test_bridge_former.py
+++ tests/unittests/models/bridgeformer/test_bridge_former.py
@@ -14,21 +14,22 @@
 
 import unittest
 import torch
-from towhee.models.frozen_in_time import FrozenInTime
+from towhee.models.bridgeformer import create_model
+from towhee.models.frozen_in_time.frozen_utils import sim_matrix
 
 
 class BridgeFormerTest(unittest.TestCase):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     # (batch，frames，channels，height， width)
-    image_size = 28
+    image_size = 32
     patch_size = 16
     in_chans = 3
+    text_len = 4
     dummy_video = torch.randn(1, 4, in_chans, image_size, image_size)
     dummy_text = {}
-    input_ids = torch.randint(1, 10, size=(1, 4))
-    attention_mask = torch.ones(1, 4, dtype=torch.int)
-    dummy_text['input_ids'] = input_ids
-    dummy_text['attention_mask'] = attention_mask
+    input_ids = torch.randint(1, 10, size=(1, text_len))
+    attention_mask = torch.ones(1, text_len, dtype=torch.int)
+    dummy_text["input_ids"] = input_ids
+    dummy_text["attention_mask"] = attention_mask
 
     def test_without_all_pretrained(self):
         '''
@@ -36,21 +37,31 @@ class BridgeFormerTest(unittest.TestCase):
         Returns:None
         '''
 
-        model = FrozenInTime(img_size=self.image_size, patch_size=self.patch_size,
+        model = create_model(pretrained=False,
+                             img_size=self.image_size, patch_size=self.patch_size,
                              in_chans=self.in_chans,
-                             attention_style='bridge_former',
-                             is_pretrained=False,
                              projection_dim=256,
-                             video_is_load_pretrained=False,
-                             video_model_type='SpaceTimeTransformer',
-                             text_is_load_pretrained=False,
-                             device=self.device)
+                             )
         text_embeddings, video_embeddings = model(text=self.dummy_text, video=self.dummy_video, return_embeds=True)
         self.assertEqual(text_embeddings.shape, (1, 256))
         self.assertEqual(video_embeddings.shape, (1, 256))
-        text_with_video_sim = model(text=self.dummy_text, video=self.dummy_video, return_embeds=False)
+        text_with_video_sim = sim_matrix(text_embeddings, video_embeddings)
+        self.assertEqual(text_with_video_sim.shape, (1, 1))
+
+    def test_without_all_pretrained_with_clip_initialized_model(self):
+        model = create_model(pretrained=False, model_name="clip_initialized_model", embed_dim=512,
+                             image_resolution=self.image_size, vision_layers=12, vision_width=768,
+                             vision_patch_size=self.patch_size,
+                             context_length=self.text_len, vocab_size=49408, transformer_width=512, transformer_heads=8,
+                             transformer_layers=12,
+                             )
+        text_embeddings = model.encode_text(self.input_ids)
+        video_embeddings = model.encode_image(self.dummy_video)
+        self.assertEqual(text_embeddings.shape, (1, 512))
+        self.assertEqual(video_embeddings.shape, (1, 512))
+        text_with_video_sim = sim_matrix(text_embeddings, video_embeddings)
         self.assertEqual(text_with_video_sim.shape, (1, 1))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
