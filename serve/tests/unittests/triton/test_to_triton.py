918dabd486576b800b10fe45c6c2f49dd9a8a608
junjiejiangjjj <junjie.jiang@zilliz.com>
2022-06-30 10:06:15 +0800

    fix triton ensemble (#1479)
    * fix triton ensemble
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>
    
    * add triton builder
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>
    
    * Adjust model format
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>
    
    * Update buidler test
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>
    
    * Add to ensemble test
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>
    
    * Add buildr test
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>
    
    * code format
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>
    
    * Fix comment
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>

--- serve/tests/unittests/triton/test_to_triton.py
+++ serve/tests/unittests/triton/test_to_triton.py
@@ -19,7 +19,7 @@ from pathlib import Path
 import filecmp
 
 from towhee import ops
-from serve.triton.to_triton_models import PyOpToTriton, PreprocessToTriton, PostprocessToTriton,ModelToTriton
+from serve.triton.to_triton_models import PyOpToTriton, PreprocessToTriton, PostprocessToTriton, ModelToTriton, EnsembleToTriton
 
 from . import EXPECTED_FILE_PATH
 
@@ -32,10 +32,10 @@ class TestPyOpToTriton(unittest.TestCase):
     def test_py_to_triton(self):
         with TemporaryDirectory(dir='./') as root:
             op = ops.local.triton_py().get_op()
-            to_triton = PyOpToTriton(op, root, 'py_to_triton_test', 'local', 'triton_py', {})
+            to_triton = PyOpToTriton(op, root, 'cb2876f3_local_triton_py', 'local', 'triton_py', {})
             to_triton.to_triton()
             expect_root = Path(EXPECTED_FILE_PATH) / 'py_to_triton_test'
-            dst = Path(root) / 'py_to_triton_test'
+            dst = Path(root) / 'cb2876f3_local_triton_py'
             self.assertTrue(filecmp.cmp(expect_root / 'config.pbtxt', dst / 'config.pbtxt'))
             self.assertTrue(filecmp.cmp(expect_root / '1' / 'model.py', dst / '1' / 'model.py'))
 
@@ -48,11 +48,11 @@ class TestPreprocessor(unittest.TestCase):
     def test_processor(self):
         with TemporaryDirectory(dir='./') as root:
             op = ops.local.triton_nnop(model_name='test').get_op()
-            to_triton = PreprocessToTriton(op, root, 'preprocess')
+            to_triton = PreprocessToTriton(op, root, 'fae9ba13_local_triton_nnop_preprocess')
             to_triton.to_triton()
 
             expect_root = Path(EXPECTED_FILE_PATH) / 'preprocess'
-            dst = Path(root) / 'preprocess'
+            dst = Path(root) / 'fae9ba13_local_triton_nnop_preprocess'
             self.assertTrue(filecmp.cmp(expect_root / 'config.pbtxt', dst / 'config.pbtxt'))
             pk = dst / '1' / 'preprocess.pickle'
             m_file = dst / '1' / 'model.py'
@@ -68,11 +68,11 @@ class TestPostprocessor(unittest.TestCase):
     def test_processor(self):
         with TemporaryDirectory(dir='./') as root:
             op = ops.local.triton_nnop(model_name='test').get_op()
-            to_triton = PostprocessToTriton(op, root, 'postprocess')
+            to_triton = PostprocessToTriton(op, root, 'fae9ba13_local_triton_nnop_postprocess')
             to_triton.to_triton()
 
             expect_root = Path(EXPECTED_FILE_PATH) / 'postprocess'
-            dst = Path(root) / 'postprocess'
+            dst = Path(root) / 'fae9ba13_local_triton_nnop_postprocess'
             self.assertTrue(filecmp.cmp(expect_root / 'config.pbtxt', dst / 'config.pbtxt'))
             pk = dst / '1' / 'postprocess.pickle'
             m_file = dst / '1' / 'model.py'
@@ -88,8 +88,51 @@ class TestToModel(unittest.TestCase):
     def test_to_model(self):
         with TemporaryDirectory(dir='./') as root:
             op = ops.local.triton_nnop(model_name='test').get_op()
-            to_triton = ModelToTriton(op.model, root, 'nnop')
+            to_triton = ModelToTriton(op, root, 'fae9ba13_local_triton_nnop_model', ['tensorrt'])
             to_triton.to_triton()
             expect_root = Path(EXPECTED_FILE_PATH) / 'nnop'
-            dst = Path(root) / 'nnop'
+            dst = Path(root) / 'fae9ba13_local_triton_nnop_model'
+            filecmp.cmp(expect_root / 'config.pbtxt', dst / 'config.pbtxt')
+
+
+class TestToEnsemble(unittest.TestCase):
+    '''
+    Test create ensemble config.
+    '''
+    test_data = {
+        'cb2876f3': {
+            'id': 'cb2876f3', 'model_name': 'cb2876f3_local_triton_py', 'model_version': 1,
+            'input': {'INPUT0': ('TYPE_STRING', [1])},
+            'output': {'OUTPUT0': ('TYPE_INT8', [-1, -1, 3]), 'OUTPUT1': ('TYPE_STRING', [1])},
+            'child_ids': ['fae9ba13']
+        },
+        'fae9ba13': {
+            'id': 'fae9ba13',
+            'model_name': 'fae9ba13_local_triton_nnop_preprocess', 'model_version': 1,
+            'input': {'INPUT0': ('TYPE_INT8', [-1, -1, 3]), 'INPUT1': ('TYPE_STRING', [1])},
+            'output': {'OUTPUT0': ('TYPE_FP32', [1, 3, 224, 224])},
+            'child_ids': ['fae9ba13_local_triton_nnop_model']
+        },
+        'fae9ba13_local_triton_nnop_model': {
+            'id': 'fae9ba13_local_triton_nnop_model',
+            'model_name': 'fae9ba13_local_triton_nnop_model', 'model_version': 1,
+            'input': {'INPUT0': ('TYPE_FP32', [-1, 3, 224, 224])},
+            'output': {'OUTPUT0': ('TYPE_FP32', [-1, 512])},
+            'child_ids': ['fae9ba13_local_triton_nnop_postprocess']
+        },
+        'fae9ba13_local_triton_nnop_postprocess': {
+            'id': 'fae9ba13_local_triton_nnop_postprocess',
+            'model_name': 'fae9ba13_local_triton_nnop_postprocess', 'model_version': 1,
+            'input': {'INPUT0': ('TYPE_FP32', [-1, 512])},
+            'output': {'OUTPUT0': ('TYPE_FP32', [512])},
+            'child_ids': []
+        }
+    }
+
+    def test_to_ensemble(self):
+        with TemporaryDirectory(dir='./') as root:
+            to_triton = EnsembleToTriton(TestToEnsemble.test_data, root, 'pipeline', 128)
+            to_triton.to_triton()
+            expect_root = Path(EXPECTED_FILE_PATH) / 'ensemble'
+            dst = Path(root) / 'pipeline'
             filecmp.cmp(expect_root / 'config.pbtxt', dst / 'config.pbtxt')
