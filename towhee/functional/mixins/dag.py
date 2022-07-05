f9c0c5e2b3da07b8e24317279abd430afe6691cc
zhujiming <jiming.zhu@zilliz.com>
2022-07-05 17:13:21 +0800

    fix start parent_ids

--- towhee/functional/mixins/dag.py
+++ towhee/functional/mixins/dag.py
@@ -152,6 +152,7 @@ class DagMixin:
                         dag[key]['op_name'] = 'towhee/' + val['call_args']['*arg'][0].function
                 else:
                     dag[key]['op_name'] = 'dummy_input'
+                    dag[key]['parent_ids'] = []
                     start = key
             else:
                 dag[key]['op_name'] = 'end'
