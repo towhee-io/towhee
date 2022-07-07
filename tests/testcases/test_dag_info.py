c652bb18a25eadf3a60de1e414e93df86799d715
朱继明 <jiming.zhu@zilliz.com>
2022-07-07 11:06:20 +0800

    fix dag_info (#1512)
    * fix dag_info
    
    * fix start
    
    * fix start parent_ids
    
    * fix dag_info testcase

--- tests/testcases/test_dag_info.py
+++ tests/testcases/test_dag_info.py
@@ -19,5 +19,11 @@ class TestDagInfo(unittest.TestCase):
 
         self.assertEqual(dc.dag_info[a]['init_args'] ,b)
 
+        for key, val in dc.dag_info.items():
+            if val['op'] == 'stream':
+                self.assertEqual(val['op_name'], 'dummy_input')
+            if val['parent_ids'] == []:
+                self.assertEqual(key, 'start')
+
 if __name__ == '__main__':
     unittest.main()
