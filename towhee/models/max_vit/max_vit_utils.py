dfef5681750b6784d8469a53fb41fa3828511dbf
xujinling <jinling.xu@zilliz.com>
2022-07-11 17:07:51 +0800

    add maxvit model utils
    Signed-off-by: xujinling <jinling.xu@zilliz.com>

---
+++ towhee/models/max_vit/max_vit_utils.py
@@ -0,0 +1,147 @@
+# Original pytorch implementation by:
+# 'MaxViT: Multi-Axis Vision Transformer'
+#       - https://arxiv.org/pdf/2204.01697.pdf
+# Original code by / Copyright 2021, Christoph Reich.
+# Modifications & additions by / Copyright 2022 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Tuple
+import torch
+from torch import nn
+
+
+def _gelu_ignore_parameters(
+        *args,
+        **kwargs
+) -> nn.Module:
+    """ Bad trick to ignore the inplace=True argument in the DepthwiseSeparableConv of Timm.
+    Args:
+        *args: Ignored.
+        **kwargs: Ignored.
+    Returns:
+        activation (nn.Module): GELU activation function.
+    """
+    activation = nn.GELU()
+    return activation
+
+
+def window_partition(
+        input: torch.Tensor,
+        window_size: Tuple[int, int] = (7, 7)
+) -> torch.Tensor:
+    """ Window partition function.
+    Args:
+        input (torch.Tensor): Input tensor of the shape [B, C, H, W].
+        window_size (Tuple[int, int], optional): Window size to be applied. Default (7, 7)
+    Returns:
+        windows (torch.Tensor): Unfolded input tensor of the shape [B * windows, window_size[0], window_size[1], C].
+    """
+    # Get size of input
+    B, C, H, W = input.shape
+    # Unfold input
+    windows = input.view(B, C, H // window_size[0], window_size[0], W // window_size[1], window_size[1])
+    # Permute and reshape to [B * windows, window_size[0], window_size[1], channels]
+    windows = windows.permute(0, 2, 4, 3, 5, 1).contiguous().view(-1, window_size[0], window_size[1], C)
+    return windows
+
+
+def window_reverse(
+        windows: torch.Tensor,
+        original_size: Tuple[int, int],
+        window_size: Tuple[int, int] = (7, 7)
+) -> torch.Tensor:
+    """ Reverses the window partition.
+    Args:
+        windows (torch.Tensor): Window tensor of the shape [B * windows, window_size[0], window_size[1], C].
+        original_size (Tuple[int, int]): Original shape.
+        window_size (Tuple[int, int], optional): Window size which have been applied. Default (7, 7)
+    Returns:
+        output (torch.Tensor): Folded output tensor of the shape [B, C, original_size[0], original_size[1]].
+    """
+    # Get height and width
+    H, W = original_size
+    # Compute original batch size
+    B = int(windows.shape[0] / (H * W / window_size[0] / window_size[1]))
+    # Fold grid tensor
+    output = windows.view(B, H // window_size[0], W // window_size[1], window_size[0], window_size[1], -1)
+    output = output.permute(0, 5, 1, 3, 2, 4).contiguous().view(B, -1, H, W)
+    return output
+
+
+def grid_partition(
+        input: torch.Tensor,
+        grid_size: Tuple[int, int] = (7, 7)
+) -> torch.Tensor:
+    """ Grid partition function.
+    Args:
+        input (torch.Tensor): Input tensor of the shape [B, C, H, W].
+        grid_size (Tuple[int, int], optional): Grid size to be applied. Default (7, 7)
+    Returns:
+        grid (torch.Tensor): Unfolded input tensor of the shape [B * grids, grid_size[0], grid_size[1], C].
+    """
+    # Get size of input
+    B, C, H, W = input.shape
+    # Unfold input
+    grid = input.view(B, C, grid_size[0], H // grid_size[0], grid_size[1], W // grid_size[1])
+    # Permute and reshape [B * (H // grid_size[0]) * (W // grid_size[1]), grid_size[0], window_size[1], C]
+    grid = grid.permute(0, 3, 5, 2, 4, 1).contiguous().view(-1, grid_size[0], grid_size[1], C)
+    return grid
+
+
+def grid_reverse(
+        grid: torch.Tensor,
+        original_size: Tuple[int, int],
+        grid_size: Tuple[int, int] = (7, 7)
+) -> torch.Tensor:
+    """ Reverses the grid partition.
+    Args:
+        Grid (torch.Tensor): Grid tensor of the shape [B * grids, grid_size[0], grid_size[1], C].
+        original_size (Tuple[int, int]): Original shape.
+        grid_size (Tuple[int, int], optional): Grid size which have been applied. Default (7, 7)
+    Returns:
+        output (torch.Tensor): Folded output tensor of the shape [B, C, original_size[0], original_size[1]].
+    """
+    # Get height, width, and channels
+    (H, W), C = original_size, grid.shape[-1]
+    # Compute original batch size
+    B = int(grid.shape[0] / (H * W / grid_size[0] / grid_size[1]))
+    # Fold grid tensor
+    output = grid.view(B, H // grid_size[0], W // grid_size[1], grid_size[0], grid_size[1], C)
+    output = output.permute(0, 5, 3, 1, 4, 2).contiguous().view(B, C, H, W)
+    return output
+
+
+def get_relative_position_index(
+        win_h: int,
+        win_w: int
+) -> torch.Tensor:
+    """ Function to generate pair-wise relative position index for each token inside the window.
+        Taken from Timms Swin V1 implementation.
+    Args:
+        win_h (int): Window/Grid height.
+        win_w (int): Window/Grid width.
+    Returns:
+        relative_coords (torch.Tensor): Pair-wise relative position indexes [height * width, height * width].
+    """
+    coords = torch.stack(torch.meshgrid([torch.arange(win_h), torch.arange(win_w)]))
+    coords_flatten = torch.flatten(coords, 1)
+    relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]
+    relative_coords = relative_coords.permute(1, 2, 0).contiguous()
+    relative_coords[:, :, 0] += win_h - 1
+    relative_coords[:, :, 1] += win_w - 1
+    relative_coords[:, :, 0] *= 2 * win_w - 1
+    return relative_coords.sum(-1)
+
+
+
+
