9881e2f5992a4568a4d8795c9d54851255afffb7
zhujiming <jiming.zhu@zilliz.com>
2022-07-12 18:38:37 +0800

    fix test pylint

--- tests/unittests/functional/test_mixins.py
+++ tests/unittests/functional/test_mixins.py
@@ -319,13 +319,13 @@ class TestRemoteMixin(unittest.TestCase):
     Unit test for RemoteMixin
     """
     def test_remote(self):
-        dc = DataCollection(((1, 2, 3, 4, 5, 6), (2, 3, 4, 5, 6, 7)))
-        remote = dc.remote('127.0.0.1:8001', mode='infer', protocol='grpc')
+        dc_1 = DataCollection(((1, 2, 3, 4, 5, 6), (2, 3, 4, 5, 6, 7)))
+        remote = dc_1.remote('127.0.0.1:8001', mode='infer', protocol='grpc')
         self.assertEqual(remote[0], None)
 
     def test_remote_error(self):
-        dc = DataCollection(((1, 2, 3, 4, 5, 6), (2, 3, 4, 5, 6, 7)))
-        remote = dc.remote('127.0.0.1:8000', mode='async_infer', protocol='http')
+        dc_2 = DataCollection(((1, 2, 3, 4, 5, 6), (2, 3, 4, 5, 6, 7)))
+        remote = dc_2.remote('127.0.0.1:8000', mode='async_infer', protocol='http')
         self.assertEqual(remote[0], None)
 
 if __name__ == '__main__':
