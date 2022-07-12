89f49a966ff5437bfc1cf4db0054a8bcc8540d4a
jinlingxu06 <106302799+jinlingxu06@users.noreply.github.com>
2022-07-12 12:40:21 +0800

    add maxvit model utils (#1540)
    Signed-off-by: xujinling <jinling.xu@zilliz.com>

---
+++ tests/unittests/models/max_vit/test_max_vit_utils.py
@@ -0,0 +1,58 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+from torch import nn
+import torch
+from towhee.models.max_vit.max_vit_utils import grid_partition, window_partition, \
+    _gelu_ignore_parameters, get_relative_position_index, window_reverse, grid_reverse
+
+
+class MaxVitUtilsTest(unittest.TestCase):
+
+    def test_grid_partition_grid_reverse(self):
+        '''
+        test the grid_partition
+        Returns:None
+        '''
+        x = torch.randn(1, 3, 28, 28)
+        out = grid_partition(x)
+        self.assertEqual(out.shape, (16, 7, 7, 3))
+        origin = grid_reverse(out, (28, 28))
+        self.assertEqual(origin.shape, (1, 3, 28, 28))
+
+    def test_window_partitionn(self):
+        '''
+        test the grid_partition
+        Returns:None
+        '''
+        x = torch.randn(1, 3, 28, 28)
+        out = window_partition(x)
+        self.assertEqual(out.shape, (16, 7, 7, 3))
+
+    def test_gelu_ignore_parameters(self):
+        out = _gelu_ignore_parameters()
+        self.assertTrue(out, nn.GELU())
+
+    def test_get_relative_position_index(self):
+        out = get_relative_position_index(win_h=24, win_w=24)
+        self.assertEqual(out.shape, (576, 576))
+
+    def test_window_reverse(self):
+        x = torch.randn(16, 7, 7, 3)
+        out = window_reverse(x, (28, 28))
+        self.assertEqual(out.shape, (1, 3, 28, 28))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ towhee/models/layers/mbconv.py
@@ -0,0 +1,94 @@
+# Original pytorch implementation by:
+# 'MaxViT: Multi-Axis Vision Transformer'
+#       - https://arxiv.org/pdf/2204.01697.pdf
+# Original code by / Copyright 2021, Christoph Reich.
+# Modifications & additions by / Copyright 2022 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Type
+import torch
+from torch import nn
+from timm.models.efficientnet_blocks import SqueezeExcite, DepthwiseSeparableConv
+from towhee.models.layers.droppath import DropPath
+from towhee.models.max_vit.max_vit_utils import _gelu_ignore_parameters
+
+
+class MBConv(nn.Module):
+    """ MBConv block as described in: https://arxiv.org/pdf/2204.01697.pdf.
+        Without downsampling:
+        x ← x + Proj(SE(DWConv(Conv(Norm(x)))))
+        With downsampling:
+        x ← Proj(Pool2D(x)) + Proj(SE(DWConv ↓(Conv(Norm(x))))).
+        Conv is a 1 X 1 convolution followed by a Batch Normalization layer and a GELU activation.
+        SE is the Squeeze-Excitation layer.
+        Proj is the shrink 1 X 1 convolution.
+        Note: This implementation differs slightly from the original MobileNet implementation!
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+        downscale (bool, optional): If true downscale by a factor of two is performed. Default: False
+        act_layer (Type[nn.Module], optional): Type of activation layer to be utilized. Default: nn.GELU
+        norm_layer (Type[nn.Module], optional): Type of normalization layer to be utilized. Default: nn.BatchNorm2d
+        drop_path (float, optional): Dropout rate to be applied during training. Default 0.
+    """
+
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            downscale: bool = False,
+            act_layer: Type[nn.Module] = nn.GELU,
+            norm_layer: Type[nn.Module] = nn.BatchNorm2d,
+            drop_path: float = 0.,
+    ) -> None:
+        """ Constructor method """
+        # Call super constructor
+        super().__init__()
+        # Save parameter
+        self.drop_path_rate: float = drop_path
+        # Check parameters for downscaling
+        if not downscale:
+            assert in_channels == out_channels, "If downscaling is utilized input and output channels must be equal."
+        # Ignore inplace parameter if GELU is used
+        if act_layer == nn.GELU:
+            act_layer = _gelu_ignore_parameters
+        # Make main path
+        self.main_path = nn.Sequential(
+            norm_layer(in_channels),
+            DepthwiseSeparableConv(in_chs=in_channels, out_chs=out_channels, stride=2 if downscale else 1,
+                                   act_layer=act_layer, norm_layer=norm_layer, drop_path_rate=drop_path),
+            SqueezeExcite(in_chs=out_channels, rd_ratio=0.25),
+            nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=(1, 1))
+        )
+        # Make skip path
+        self.skip_path = nn.Sequential(
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(1, 1))
+        ) if downscale else nn.Identity()
+        self.drop_path = DropPath(self.drop_path_rate)
+
+    def forward(
+            self,
+            data: torch.Tensor
+    ) -> torch.Tensor:
+        """ Forward pass.
+        Args:
+            data (torch.Tensor): Input tensor of the shape [B, C_in, H, W].
+        Returns:
+            output (torch.Tensor): Output tensor of the shape [B, C_out, H (// 2), W (// 2)] (downscaling is optional).
+        """
+        output = self.main_path(data)
+        if self.drop_path_rate > 0.:
+            output = self.drop_path(output)
+        output = output + self.skip_path(data)
+        return output
