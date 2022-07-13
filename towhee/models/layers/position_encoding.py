53137cdc03e96ae290167733d5f6f943a5757851
gexy5 <xinyu.ge@zilliz.com>
2022-07-13 15:21:06 +0800

    add position encoding
    Signed-off-by: gexy5 <xinyu.ge@zilliz.com>
    
    add position encoding
    
    Signed-off-by: gexy5 <xinyu.ge@zilliz.com>
    
    add position encoding
    
    Signed-off-by: gexy5 <xinyu.ge@zilliz.com>

--- towhee/models/layers/position_encoding.py
+++ towhee/models/layers/position_encoding.py
@@ -22,6 +22,7 @@ Various positional encodings for the transformer.
 import math
 import torch
 from torch import nn
+from torch.autograd import Variable
 
 from towhee.models.coformer.utils import NestedTensor
 
@@ -96,3 +97,28 @@ def build_position_encoding(
         raise ValueError(f'not supported {position_embedding}')
 
     return position_embedding
+
+class PositionalEncoding(nn.Module):
+    """
+    Positional encoding of the strm model.
+    """
+
+    def __init__(self, d_model, dropout, max_len=5000, pe_scale_factor=0.1):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        self.pe_scale_factor = pe_scale_factor
+        # Compute the positional encodings once in log space.
+        # pe is of shape max_len(5000) x 2048(last layer of FC)
+        pe = torch.zeros(max_len, d_model)
+        # position is of shape 5000 x 1
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term) * self.pe_scale_factor
+        pe[:, 1::2] = torch.cos(position * div_term) * self.pe_scale_factor
+        # pe contains a vector of shape 1 x 5000 x 2048
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + Variable(self.pe[:, :x.size(1)], requires_grad=False)
+        return self.dropout(x)
