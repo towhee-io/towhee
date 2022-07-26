787a6a7a7d26e0fe0d77bd57a0592ef71956afb2
zhujiming <jiming.zhu@zilliz.com>
2022-07-26 10:38:06 +0800

    add example of dag info to UT

--- tests/testcases/test_dag_info.py
+++ tests/testcases/test_dag_info.py
@@ -8,6 +8,72 @@ class TestDagInfo(unittest.TestCase):
     def test_image_decode_clip_dag_information(self):
         """
         Unittest dag info
+
+        example of dag info
+        {
+            'e4b074eb': {
+                'op': 'map',
+                'op_name': 'towhee/image-decode',
+                'is_stream': True,
+                'init_args': {},
+                'call_args': {
+                    '*arg': ( < towhee.engine.factory._OperatorLazyWrapper object at 0x1216d4f70 > , ),
+                    '*kws': {}
+                },
+                'op_config': None,
+                'input_info': [('start', 'path')],
+                'output_info': ['img'],
+                'parent_ids': ['start'],
+                'child_ids': ['db5377c3']
+            },
+            'db5377c3': {
+                'op': 'map',
+                'op_name': 'towhee/clip',
+                'is_stream': True,
+                'init_args': {
+                    'model_name': 'clip_vit_b32',
+                    'modality': 'image'
+                },
+                'call_args': {
+                    '*arg': ( < towhee.engine.factory._OperatorLazyWrapper object at 0x12171a670 > , ),
+                    '*kws': {}
+                },
+                'op_config': {
+                    'ac': '123',
+                    'asd': 'wea'
+                },
+                'input_info': [('e4b074eb', 'img')],
+                'output_info': ['vec'],
+                'parent_ids': ['e4b074eb'],
+                'child_ids': ['end']
+            },
+            'end': {
+                'op': 'end',
+                'op_name': 'end',
+                'init_args': None,
+                'call_args': None,
+                'op_config': None,
+                'input_info': None,
+                'output_info': None,
+                'parent_ids': ['db5377c3'],
+                'child_ids': []
+            },
+            'start': {
+                'op': 'stream',
+                'op_name': 'dummy_input',
+                'is_stream': False,
+                'init_args': None,
+                'call_args': {
+                    '*arg': (),
+                    '*kws': {}
+                },
+                'op_config': None,
+                'input_info': None,
+                'output_info': None,
+                'parent_ids': [],
+                'child_ids': ['e4b074eb']
+            }
+        }
         """
         dc = towhee.dummy_input() \
                 .image_decode['path', 'img']() \
