b90e798b340a1618565cf856c72c46654dd1f8e2
zhujiming <jiming.zhu@zilliz.com>
2022-08-02 00:43:14 +0800

    fix test

--- tests/unittests/triton/test_to_triton.py
+++ tests/unittests/triton/test_to_triton.py
@@ -32,10 +32,10 @@ class TestPyOpToTriton(unittest.TestCase):
     def test_py_to_triton(self):
         with TemporaryDirectory(dir='./') as root:
             op = ops.local.triton_py().get_op()
-            to_triton = PyOpToTriton(op, root, 'cb2876f3_local_triton_py', 'local', 'triton_py', {}, {'device_ids': [1]})
+            to_triton = PyOpToTriton(op, root, 'e4b074eb_local_triton_py', 'local', 'triton_py', {}, {'device_ids': [1]})
             to_triton.to_triton()
             expect_root = Path(EXPECTED_FILE_PATH) / 'py_to_triton_test'
-            dst = Path(root) / 'cb2876f3_local_triton_py'
+            dst = Path(root) / 'e4b074eb_local_triton_py'
             self.assertTrue(filecmp.cmp(expect_root / 'config.pbtxt', dst / 'config.pbtxt'))
             self.assertTrue(filecmp.cmp(expect_root / '1' / 'model.py', dst / '1' / 'model.py'))
 
@@ -48,7 +48,7 @@ class TestPreprocessor(unittest.TestCase):
     def test_processor(self):
         with TemporaryDirectory(dir='./') as root:
             op = ops.local.triton_nnop(model_name='test').get_op()
-            to_triton = PreprocessToTriton(op, root, 'fae9ba13_local_triton_nnop_preprocess',
+            to_triton = PreprocessToTriton(op, root, 'db5377c3_local_triton_nnop_preprocess',
                                            {
                                                'dynamic_batching': {
                                                    'max_batch_size': 128,
@@ -59,7 +59,7 @@ class TestPreprocessor(unittest.TestCase):
                                            })
             to_triton.to_triton()
             expect_root = Path(EXPECTED_FILE_PATH) / 'preprocess'
-            dst = Path(root) / 'fae9ba13_local_triton_nnop_preprocess'
+            dst = Path(root) / 'db5377c3_local_triton_nnop_preprocess'
             self.assertTrue(filecmp.cmp(expect_root / 'config.pbtxt', dst / 'config.pbtxt'))
             pk = dst / '1' / 'preprocess.pickle'
             m_file = dst / '1' / 'model.py'
@@ -75,7 +75,7 @@ class TestPostprocessor(unittest.TestCase):
     def test_processor(self):
         with TemporaryDirectory(dir='./') as root:
             op = ops.local.triton_nnop(model_name='test').get_op()
-            to_triton = PostprocessToTriton(op, root, 'fae9ba13_local_triton_nnop_postprocess',
+            to_triton = PostprocessToTriton(op, root, 'db5377c3_local_triton_nnop_postprocess',
                                             {
                                                'dynamic_batching': {
                                                    'max_batch_size': 128,
@@ -87,7 +87,7 @@ class TestPostprocessor(unittest.TestCase):
             to_triton.to_triton()
 
             expect_root = Path(EXPECTED_FILE_PATH) / 'postprocess'
-            dst = Path(root) / 'fae9ba13_local_triton_nnop_postprocess'
+            dst = Path(root) / 'db5377c3_local_triton_nnop_postprocess'
             self.assertTrue(filecmp.cmp(expect_root / 'config.pbtxt', dst / 'config.pbtxt'))
             pk = dst / '1' / 'postprocess.pickle'
             m_file = dst / '1' / 'model.py'
@@ -103,7 +103,7 @@ class TestToModel(unittest.TestCase):
     def test_to_model(self):
         with TemporaryDirectory(dir='./') as root:
             op = ops.local.triton_nnop(model_name='test').get_op()
-            to_triton = ModelToTriton(op, root, 'fae9ba13_local_triton_nnop_model', ['tensorrt'],
+            to_triton = ModelToTriton(op, root, 'db5377c3_local_triton_nnop_model', ['tensorrt'],
                                            {
                                                'device_ids': [1, 2],
                                                'instance_count': 2,
@@ -115,7 +115,7 @@ class TestToModel(unittest.TestCase):
                                            })
             to_triton.to_triton()
             expect_root = Path(EXPECTED_FILE_PATH) / 'nnop'
-            dst = Path(root) / 'fae9ba13_local_triton_nnop_model'
+            dst = Path(root) / 'db5377c3_local_triton_nnop_model'
             self.assertTrue(filecmp.cmp(expect_root / 'config.pbtxt', dst / 'config.pbtxt'))
 
 
@@ -128,28 +128,32 @@ class TestToEnsemble(unittest.TestCase):
             'id': 'cb2876f3', 'model_name': 'cb2876f3_local_triton_py', 'model_version': 1,
             'input': {'INPUT0': ('TYPE_STRING', [1])},
             'output': {'OUTPUT0': ('TYPE_INT8', [-1, -1, 3]), 'OUTPUT1': ('TYPE_STRING', [1])},
-            'child_ids': ['fae9ba13']
+            'child_ids': ['fae9ba13'],
+            'parent_ids': ['start']
         },
         'fae9ba13': {
             'id': 'fae9ba13',
             'model_name': 'fae9ba13_local_triton_nnop_preprocess', 'model_version': 1,
             'input': {'INPUT0': ('TYPE_INT8', [-1, -1, 3]), 'INPUT1': ('TYPE_STRING', [1])},
-            'output': {'OUTPUT0': ('TYPE_FP32', [1, 3, 224, 224])},
-            'child_ids': ['fae9ba13_local_triton_nnop_model']
+            'output': {'OUTPUT0': ('TYPE_FP32', [-1, 3, 224, 224])},
+            'child_ids': ['fae9ba13_local_triton_nnop_model'],
+            'parent_ids': ['cb2876f3']
         },
         'fae9ba13_local_triton_nnop_model': {
             'id': 'fae9ba13_local_triton_nnop_model',
             'model_name': 'fae9ba13_local_triton_nnop_model', 'model_version': 1,
             'input': {'INPUT0': ('TYPE_FP32', [-1, 3, 224, 224])},
             'output': {'OUTPUT0': ('TYPE_FP32', [-1, 512])},
-            'child_ids': ['fae9ba13_local_triton_nnop_postprocess']
+            'child_ids': ['fae9ba13_local_triton_nnop_postprocess'],
+            'parent_ids': ['fae9ba13']
         },
         'fae9ba13_local_triton_nnop_postprocess': {
             'id': 'fae9ba13_local_triton_nnop_postprocess',
             'model_name': 'fae9ba13_local_triton_nnop_postprocess', 'model_version': 1,
             'input': {'INPUT0': ('TYPE_FP32', [-1, 512])},
             'output': {'OUTPUT0': ('TYPE_FP32', [512])},
-            'child_ids': []
+            'child_ids': [],
+            'parent_ids': ['fae9ba13_local_triton_nnop_model']
         }
     }
 
