fbce4b120b65621291982d06e40d27dc8c1d4196
Jael Gu <mengjia.gu@zilliz.com>
2022-08-17 19:57:34 +0800

    Add docstring to Mlp layer
    Signed-off-by: Jael Gu <mengjia.gu@zilliz.com>

--- towhee/models/layers/mlp.py
+++ towhee/models/layers/mlp.py
@@ -13,16 +13,38 @@
 # limitations under the License.
 # This code is modified by Zilliz.
 
-""" MLP module w/ dropout and configurable activation layer
-Hacked together by / Copyright 2020 Ross Wightman
-"""
+
 from torch import nn
 
 from towhee.models.layers.layers_with_relprop import GELU, Linear, Dropout
 
 
 class Mlp(nn.Module):
-    """ MLP as used in Vision Transformer, MLP-Mixer and related networks
+    """
+    MLP module w/ dropout and configurable activation layer,
+    as used in Vision Transformer, MLP-Mixer and related networks.
+
+    Args:
+        - in_features (`int`):
+            Number of input features.
+        - hidden_features (`int`):
+            Number of hidden features.
+        - out_features (`int`):
+            Number of output features.
+        - act_layer (`nn.Module`):
+            Activation layer.
+        - drop (`float`):
+            Dropout rate.
+
+    Example:
+        >>> import torch
+        >>> from towhee.models.layers.mlp import Mlp
+        >>>
+        >>> fake_input = torch.rand(1, 4)
+        >>> layer = Mlp(in_features=4, hidden_features=6, out_features=8)
+        >>> output = layer(fake_input)
+        >>> print(output.shape)
+        torch.Size([1, 8])
     """
 
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=GELU, drop=0.):
