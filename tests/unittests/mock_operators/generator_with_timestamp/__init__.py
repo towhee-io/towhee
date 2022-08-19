5ef22a4ac70127d583d2891d2f5570d1cbcdc328
junjie.jiang <junjie.jiang@zilliz.com>
2022-08-19 15:14:55 +0800

    [Serve] generator_with_timestamp op support triton
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>

--- tests/unittests/mock_operators/generator_with_timestamp/__init__.py
+++ tests/unittests/mock_operators/generator_with_timestamp/__init__.py
@@ -11,3 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+from generator_with_timestamp import GeneratorWithTimestamp
+
+
+def generator_with_timestamp():
+    return GeneratorWithTimestamp()
