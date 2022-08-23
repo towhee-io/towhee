92cdbdb12bcc6c79f7b4126a03023127158655e5
jie.hou <jie.hou@zilliz.com>
2022-08-23 14:52:50 +0800

    drop unused code
    Signed-off-by: jie.hou <jie.hou@zilliz.com>

--- towhee/utils/__init__.py
+++ towhee/utils/__init__.py
@@ -11,11 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-from towhee.utils.handler_mixin import HandlerMixin
-
-
-__all__ = [
-    'HandlerMixin',
-]
