f0175c102df50ace9b03ce25cf01e9a9b677a58d
Filip Haltmayer <filip.haltmayer@zilliz.com>
2022-08-23 13:10:17 -0700

    Init docstring updates
    Signed-off-by: Filip Haltmayer <filip.haltmayer@zilliz.com>

--- towhee/functional/__init__.py
+++ towhee/functional/__init__.py
@@ -32,22 +32,24 @@ read_zip = DataCollection.read_zip
 
 # pylint: disable=redefined-builtin
 def _range(*args, **kwargs):  # pragma: no cover
-    """
-    Return a DataCollection of a range of values.
+    """Return a DataCollection of a range of values.
+
+    Returns:
+        DataCollection or DataFrame: The range of values.
+
     Examples:
 
-    1. create a simple data collection;
+        1. create a simple data collection;
 
-    >>> import towhee
-    >>> towhee.range(5).to_list() #doctest: +SKIP
-    [0, 1, 2, 3, 4]
+        >>> import towhee
+        >>> towhee.range(5).to_list() #doctest: +SKIP
+        [0, 1, 2, 3, 4]
 
-    2. create a data collection of schema'd range.
+        2. create a data collection of schema'd range.
 
-    >>> towhee.range['nums'](5).select['nums']().as_raw() #doctest: +SKIP
-    [0, 1, 2, 3, 4]
+        >>> towhee.range['nums'](5).select['nums']().as_raw() #doctest: +SKIP
+        [0, 1, 2, 3, 4]
     """
-
     index = param_scope()._index
     if index is None:
         return DataCollection.range(*args, **kwargs)
@@ -58,20 +60,23 @@ range = dynamic_dispatch(_range)
 
 
 def _glob(*arg):  # pragma: no cover
-    """
-    Return a DataCollection of paths matching a pathname pattern.
+    """Return a DataCollection of paths matching a pathname pattern.
+
+    Returns:
+        DataCollection or DataFrame: DataCollection holding paths.
+
     Examples:
 
-    1. create a simple data collection;
+        1. create a simple data collection;
 
-    >>> import towhee
-    >>> towhee.glob('*.jpg', '*.png').to_list() #doctest: +SKIP
-    ['a.jpg', 'b.jpg', 'x.png']
+        >>> import towhee
+        >>> towhee.glob('*.jpg', '*.png').to_list() #doctest: +SKIP
+        ['a.jpg', 'b.jpg', 'x.png']
 
-    2. create a data collection of structural data.
+        2. create a data collection of structural data.
 
-    >>> towhee.glob['path']('*.jpg').to_list() #doctest: +SKIP
-    [<Entity dict_keys(['path'])>, <Entity dict_keys(['path'])>]
+        >>> towhee.glob['path']('*.jpg').to_list() #doctest: +SKIP
+        [<Entity dict_keys(['path'])>, <Entity dict_keys(['path'])>]
     """
 
     index = param_scope()._index
@@ -84,20 +89,27 @@ glob = dynamic_dispatch(_glob)
 
 
 def _glob_zip(uri, pattern):  # pragma: no cover
-    """
-    Return a DataCollection of files matching a pathname pattern from a zip archive.
+    """Return a DataCollection of files matching a pathname pattern from a zip archive.
+
+    Args:
+        uri (str or Path): The path to zip.
+        pattern (str): The pattern to match.
+
+    Returns:
+        DataCollection or DataFrame: DataCollection holding paths.
+
     Examples:
 
-    1. create a simple data collection;
+        1. create a simple data collection;
 
-    >>> import towhee
-    >>> towhee.glob_zip('somefile.zip', '*.jpg').to_list() #doctest: +SKIP
-    ['a.jpg', 'b.jpg]
+        >>> import towhee
+        >>> towhee.glob_zip('somefile.zip', '*.jpg').to_list() #doctest: +SKIP
+        ['a.jpg', 'b.jpg]
 
-    2. create a data collection of structural data.
+        2. create a data collection of structural data.
 
-    >>> towhee.glob_zip['path']('somefile.zip', '*.jpg').to_list() #doctest: +SKIP
-    [<Entity dict_keys(['path'])>, <Entity dict_keys(['path'])>]
+        >>> towhee.glob_zip['path']('somefile.zip', '*.jpg').to_list() #doctest: +SKIP
+        [<Entity dict_keys(['path'])>, <Entity dict_keys(['path'])>]
     """
 
     index = param_scope()._index
@@ -110,8 +122,11 @@ glob_zip = dynamic_dispatch(_glob_zip)
 
 
 def _api():
-    """
-    Create an API input, for building RestFul API or application API.
+    """Create an API input, for building RestFul API or application API.
+
+    Returns:
+        DataFrame: DataFrame containing the APi.
+
 
     Examples:
 
@@ -159,42 +174,46 @@ api = dynamic_dispatch(_api)
 
 
 def _dummy_input():
-    """
-    Create a dummy input.
+    """Create a dummy input.
 
-    >>> from fastapi import FastAPI
-    >>> from fastapi.testclient import TestClient
-    >>> app = FastAPI()
+    Returns:
+        DataFrame: DataFrame containing the pipeline.
 
-    >>> import towhee
-    >>> app1 = (
-    ...     towhee.dummy_input().map(lambda x: x+' -> 1')
-    ...        .map(lambda x: x+' => 1')
-    ...        .serve('/app1', app)
-    ... )
+    Examples:
 
-    >>> app2 = (
-    ...     towhee.dummy_input['x']().runas_op['x', 'x_plus_1'](func=lambda x: x+' -> 2')
-    ...        .runas_op['x_plus_1', 'y'](func=lambda x: x+' => 2')
-    ...        .select['y']()
-    ...        .serve('/app2', app)
-    ... )
+        >>> from fastapi import FastAPI
+        >>> from fastapi.testclient import TestClient
+        >>> app = FastAPI()
 
-    >>> app2 = (
-    ...     towhee.dummy_input().parse_json()
-    ...        .runas_op['x', 'x_plus_1'](func=lambda x: x+' -> 3')
-    ...        .runas_op['x_plus_1', 'y'](func=lambda x: x+' => 3')
-    ...        .select['y']()
-    ...        .serve('/app3', app)
-    ... )
+        >>> import towhee
+        >>> app1 = (
+        ...     towhee.dummy_input().map(lambda x: x+' -> 1')
+        ...        .map(lambda x: x+' => 1')
+        ...        .serve('/app1', app)
+        ... )
 
-    >>> client = TestClient(app)
-    >>> client.post('/app1', '1').text
-    '"1 -> 1 => 1"'
-    >>> client.post('/app2', '2').text
-    '{"y":"2 -> 2 => 2"}'
-    >>> client.post('/app3', '{"x": "3"}').text
-    '{"y":"3 -> 3 => 3"}'
+        >>> app2 = (
+        ...     towhee.dummy_input['x']().runas_op['x', 'x_plus_1'](func=lambda x: x+' -> 2')
+        ...        .runas_op['x_plus_1', 'y'](func=lambda x: x+' => 2')
+        ...        .select['y']()
+        ...        .serve('/app2', app)
+        ... )
+
+        >>> app2 = (
+        ...     towhee.dummy_input().parse_json()
+        ...        .runas_op['x', 'x_plus_1'](func=lambda x: x+' -> 3')
+        ...        .runas_op['x_plus_1', 'y'](func=lambda x: x+' => 3')
+        ...        .select['y']()
+        ...        .serve('/app3', app)
+        ... )
+
+        >>> client = TestClient(app)
+        >>> client.post('/app1', '1').text
+        '"1 -> 1 => 1"'
+        >>> client.post('/app2', '2').text
+        '{"y":"2 -> 2 => 2"}'
+        >>> client.post('/app3', '{"x": "3"}').text
+        '{"y":"3 -> 3 => 3"}'
     """
     return _api().__enter__()
 
@@ -203,24 +222,29 @@ dummy_input = dynamic_dispatch(_dummy_input)
 
 
 def _dc(iterable):
-    """
-    Return a DataCollection.
+    """Return a DataCollection.
+
+    Args:
+        iterable (iterable): The iterable to wrap in a DataCollection.
+
+    Returns:
+        DataCollection or DataFrame: The wrapped iterable.
 
     Examples:
 
-    1. create a simple data collection;
+        1. create a simple data collection;
 
-    >>> import towhee
-    >>> towhee.dc([0, 1, 2]).to_list()
-    [0, 1, 2]
+        >>> import towhee
+        >>> towhee.dc([0, 1, 2]).to_list()
+        [0, 1, 2]
 
-    2. create a data collection of structural data.
+        2. create a data collection of structural data.
 
-    >>> towhee.dc['column']([0, 1, 2]).to_list()
-    [<Entity dict_keys(['column'])>, <Entity dict_keys(['column'])>, <Entity dict_keys(['column'])>]
+        >>> towhee.dc['column']([0, 1, 2]).to_list()
+        [<Entity dict_keys(['column'])>, <Entity dict_keys(['column'])>, <Entity dict_keys(['column'])>]
 
-    >>> towhee.dc['string', 'int']([['a', 1], ['b', 2], ['c', 3]]).to_list()
-    [<Entity dict_keys(['string', 'int'])>, <Entity dict_keys(['string', 'int'])>, <Entity dict_keys(['string', 'int'])>]
+        >>> towhee.dc['string', 'int']([['a', 1], ['b', 2], ['c', 3]]).to_list()
+        [<Entity dict_keys(['string', 'int'])>, <Entity dict_keys(['string', 'int'])>, <Entity dict_keys(['string', 'int'])>]
     """
 
     index = param_scope()._index
