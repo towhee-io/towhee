641c7c793b5c299f0db5e78d5018f81f967cc21d
junjie.jiang <junjie.jiang@zilliz.com>
2022-08-23 14:00:32 +0800

    mock repeat op support serve
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>

--- tests/unittests/mock_operators/repeat_operator/repeat_operator.py
+++ tests/unittests/mock_operators/repeat_operator/repeat_operator.py
@@ -36,3 +36,9 @@ class RepeatOperator(Operator):
     @property
     def shared_type(self):
         return SharedType.Shareable
+
+    def input_schema(self):
+        return [(int, (1, ))]
+
+    def output_schema(self):
+        return [(int, (1, ))]
