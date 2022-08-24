3dfe8ba7a0d9d8dc9e7b38c8a1ac7d1cb5300416
Filip Haltmayer <81822489+filip-halt@users.noreply.github.com>
2022-08-24 16:30:53 -0700

    Update to config docstring (#1882)
    Signed-off-by: Filip Haltmayer <filip.haltmayer@zilliz.com>
    
    Signed-off-by: Filip Haltmayer <filip.haltmayer@zilliz.com>

--- towhee/functional/mixins/config.py
+++ towhee/functional/mixins/config.py
@@ -17,44 +17,43 @@ from towhee.hparam import param_scope
 
 
 class ConfigMixin:
-    """
-    Mixin to config DC, such as set the `parallel`, `chunksize`, `jit`.
+    """Mixin to config DC, such as set the `parallel`, `chunksize`, `jit`.
 
     Examples:
 
-    >>> import towhee
-    >>> dc = towhee.dc['a'](range(20))
-    >>> dc = dc.set_chunksize(10)
-    >>> dc = dc.set_parallel(2)
-    >>> dc = dc.set_jit('numba')
-    >>> dc.get_config()
-    {'parallel': 2, 'chunksize': 10, 'jit': 'numba', 'format_priority': None}
-    >>> dc1 = towhee.dc([1,2,3]).config(jit='numba')
-    >>> dc2 = towhee.dc['a'](range(40)).config(parallel=2, chunksize=20)
-    >>> dc1.get_config()
-    {'parallel': None, 'chunksize': None, 'jit': 'numba', 'format_priority': None}
-    >>> dc2.get_config()
-    {'parallel': 2, 'chunksize': 20, 'jit': None, 'format_priority': None}
-    >>> dc3 = towhee.dc['a'](range(10)).config(format_priority=['tensorrt', 'onnx'])
-    >>> dc3.get_config()
-    {'parallel': None, 'chunksize': None, 'jit': None, 'format_priority': ['tensorrt', 'onnx']}
+        >>> import towhee
+        >>> dc = towhee.dc['a'](range(20))
+        >>> dc = dc.set_chunksize(10)
+        >>> dc = dc.set_parallel(2)
+        >>> dc = dc.set_jit('numba')
+        >>> dc.get_config()
+        {'parallel': 2, 'chunksize': 10, 'jit': 'numba', 'format_priority': None}
+        >>> dc1 = towhee.dc([1,2,3]).config(jit='numba')
+        >>> dc2 = towhee.dc['a'](range(40)).config(parallel=2, chunksize=20)
+        >>> dc1.get_config()
+        {'parallel': None, 'chunksize': None, 'jit': 'numba', 'format_priority': None}
+        >>> dc2.get_config()
+        {'parallel': 2, 'chunksize': 20, 'jit': None, 'format_priority': None}
+        >>> dc3 = towhee.dc['a'](range(10)).config(format_priority=['tensorrt', 'onnx'])
+        >>> dc3.get_config()
+        {'parallel': None, 'chunksize': None, 'jit': None, 'format_priority': ['tensorrt', 'onnx']}
 
-    >>> import towhee
-    >>> dc = towhee.dc['a'](range(20))
-    >>> dc = dc.set_chunksize(10)
-    >>> dc = dc.set_parallel(2)
-    >>> dc = dc.set_jit('numba')
-    >>> dc.get_pipeline_config()
-    {'parallel': 2, 'chunksize': 10, 'jit': 'numba', 'format_priority': None}
-    >>> dc1 = towhee.dc([1,2,3]).pipeline_config(jit='numba')
-    >>> dc2 = towhee.dc['a'](range(40)).pipeline_config(parallel=2, chunksize=20)
-    >>> dc1.get_pipeline_config()
-    {'parallel': None, 'chunksize': None, 'jit': 'numba', 'format_priority': None}
-    >>> dc2.get_pipeline_config()
-    {'parallel': 2, 'chunksize': 20, 'jit': None, 'format_priority': None}
-    >>> dc3 = towhee.dc['a'](range(10)).pipeline_config(format_priority=['tensorrt', 'onnx'])
-    >>> dc3.get_pipeline_config()
-    {'parallel': None, 'chunksize': None, 'jit': None, 'format_priority': ['tensorrt', 'onnx']}
+        >>> import towhee
+        >>> dc = towhee.dc['a'](range(20))
+        >>> dc = dc.set_chunksize(10)
+        >>> dc = dc.set_parallel(2)
+        >>> dc = dc.set_jit('numba')
+        >>> dc.get_pipeline_config()
+        {'parallel': 2, 'chunksize': 10, 'jit': 'numba', 'format_priority': None}
+        >>> dc1 = towhee.dc([1,2,3]).pipeline_config(jit='numba')
+        >>> dc2 = towhee.dc['a'](range(40)).pipeline_config(parallel=2, chunksize=20)
+        >>> dc1.get_pipeline_config()
+        {'parallel': None, 'chunksize': None, 'jit': 'numba', 'format_priority': None}
+        >>> dc2.get_pipeline_config()
+        {'parallel': 2, 'chunksize': 20, 'jit': None, 'format_priority': None}
+        >>> dc3 = towhee.dc['a'](range(10)).pipeline_config(format_priority=['tensorrt', 'onnx'])
+        >>> dc3.get_pipeline_config()
+        {'parallel': None, 'chunksize': None, 'jit': None, 'format_priority': ['tensorrt', 'onnx']}
     """
 
     def __init__(self) -> None:
@@ -75,19 +74,18 @@ class ConfigMixin:
             self._format_priority = None
 
     def config(self, parallel: int = None, chunksize: int = None, jit: Union[str, dict] = None, format_priority: List[str] = None):
-        """
-        Set the parameters in DC.
+        """ Set the parameters for the DC.
 
         Args:
-            parallel (`int`):
-               Set the number of parallel execution for following calls.
-            chunksize (`int`):
-               Set the chunk size for arrow.
-            jit (`Union[str, dict]`):
-               It can set to "numba", this mode will speed up the Operator's function, but it may also need to return to python mode due to JIT
-               failure, which will take longer, so please set it carefully.
-            format_priority (`List[str]`):
-                The priority list of format.
+            parallel (int, optional): Set the number of parallel execution for the following calls. Defaults to None.
+            chunksize (int, optional): Set the chunk size for arrow.. Defaults to None.
+            jit (Union[str, dict], optional): Can be set to "numba", this mode will speed up the Operator's function,
+                but it may also need to return to python mode due to JIT failure, which will take longer, so please
+                set it carefully. Defaults to None.
+            format_priority (List[str], optional): The priority list of formats. Defaults to None.
+
+        Returns:
+            DataCollection: Self.
         """
         dc = self
         if jit is not None:
@@ -101,8 +99,10 @@ class ConfigMixin:
         return dc
 
     def get_config(self):
-        """
-        Return the config in DC, such as `parallel`, `chunksize`, `jit` and `format_priority`.
+        """Return the config of the DC, including parameters such as `parallel`, `chunksize`, `jit` and `format_priority`.
+
+        Returns:
+            dict: A dict of config parameters.
         """
         self._config = {}
 
@@ -117,19 +117,18 @@ class ConfigMixin:
         return self._config
 
     def pipeline_config(self, parallel: int = None, chunksize: int = None, jit: Union[str, dict] = None, format_priority: List[str] = None):
-        """
-        Set the parameters in DC.
+        """Set the parameters in DC.
 
         Args:
-            parallel (`int`):
-               Set the number of parallel execution for following calls.
-            chunksize (`int`):
-               Set the chunk size for arrow.
-            jit (`Union[str, dict]`):
-               It can set to "numba", this mode will speed up the Operator's function, but it may also need to return to python mode due to JIT
-               failure, which will take longer, so please set it carefully.
-            format_priority (`List[str]`):
-                The priority list of format.
+            parallel (int, optional): Set the number of parallel executions for the following calls. Defaults to None.
+            chunksize (int, optional): Set the chunk size for arrow. Defaults to None.
+            jit (Union[str, dict], optional): Can be set to "numba", this mode will speed up the Operator's function,
+                but it may also need to return to python mode due to JIT failure, which will take longer, so please
+                set it carefully. Defaults to None.
+            format_priority (List[str], optional): The priority list of format. Defaults to None.
+
+        Returns:
+            DataCollection: Self
         """
         dc = self
         if jit is not None:
@@ -143,8 +142,10 @@ class ConfigMixin:
         return dc
 
     def get_pipeline_config(self):
-        """
-        Return the config in DC, such as `parallel`, `chunksize`, `jit` and `format_priority`.
+        """Return the config of the DC, including parameters such as `parallel`, `chunksize`, `jit` and `format_priority`.
+
+        Returns:
+            dict: A dict of config parameters.
         """
         self._pipeline_config = {}
 
