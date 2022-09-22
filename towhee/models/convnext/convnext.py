6ed809ce3d17899f4b6f623c04550c47b2f6b863
Jael Gu <mengjia.gu@zilliz.com>
2022-09-22 15:39:09 +0800

    Add configs & API to create ConvNeXt by model name
    Signed-off-by: Jael Gu <mengjia.gu@zilliz.com>

--- towhee/models/convnext/convnext.py
+++ towhee/models/convnext/convnext.py
@@ -19,6 +19,7 @@ import torch
 from torch import nn
 
 from towhee.models.convnext.utils import LayerNorm, Block
+from towhee.models.convnext.configs import get_configs
 from towhee.models.utils.weight_init import trunc_normal_
 
 
@@ -47,15 +48,19 @@ class ConvNeXt(nn.Module):
                  ):
         super().__init__()
 
+        self.depths = depths
+        self.dims = dims
+        self.num_classes = num_classes
+
         self.downsample_layers = nn.ModuleList()  # stem and 3 intermediate downsampling conv layers
         stem = nn.Sequential(
             nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
-            LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
+            LayerNorm(dims[0], eps=1e-6, data_format='channels_first')
         )
         self.downsample_layers.append(stem)
         for i in range(3):
             downsample_layer = nn.Sequential(
-                LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
+                LayerNorm(dims[i], eps=1e-6, data_format='channels_first'),
                 nn.Conv2d(dims[i], dims[i + 1], kernel_size=2, stride=2),
             )
             self.downsample_layers.append(downsample_layer)
@@ -93,3 +98,37 @@ class ConvNeXt(nn.Module):
         x = self.forward_features(x)
         x = self.head(x)
         return x
+
+
+def create_model(
+        model_name: str = None,
+        pretrained: bool = False,
+        weights_path: str = None,
+        device: str = None,
+        **kwargs,
+):
+    if device is None:
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    configs = get_configs(model_name)
+    configs.update(**kwargs)
+    if 'url' in configs:
+        url = configs['url']
+        del configs['url']
+    else:
+        url = None
+
+    model = ConvNeXt(**configs).to(device)
+
+    if pretrained:
+        if weights_path is None:
+            assert url, 'No default url or weights path is provided for the pretrained model.'
+            state_dict = torch.hub.load_state_dict_from_url(url=url, map_location=device)
+        else:
+            state_dict = torch.load(weights_path, map_location=device)
+        if 'model' in state_dict:
+            state_dict = state_dict['model']
+        model.load_state_dict(state_dict)
+
+    model.eval()
+    return model
