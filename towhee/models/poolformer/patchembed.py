13468192a03da3292a12478ce701862723a2fd35
Junchen He <hovmey@hotmail.com>
2022-09-28 20:36:54 +0800

    add patchembed specified by poolformer (#2158)
    Co-authored-by: Junxen <Junxen@users.noreply.github.com>

---
+++ towhee/models/poolformer/patchembed.py
@@ -0,0 +1,40 @@
+# Built on top of the original implementation at https://github.com/sail-sg/poolformer/blob/main/models/poolformer.py
+#
+# Modifications by Copyright 2022 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from torch import nn
+
+
+class PatchEmbed(nn.Module):
+    """
+    Patch Embedding that is implemented by a layer of conv, this is specified by poolformer
+    Input: tensor in shape [B, C, H, W]
+    Output: tensor in shape [B, C, H/stride, W/stride]
+    """
+    def __init__(self, patch_size=16, stride=16, padding=0,
+                 in_chans=3, embed_dim=768, norm_layer=None):
+        super().__init__()
+        patch_size = (patch_size, patch_size)
+        stride = (stride, stride)
+        padding = (padding, padding)
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size,
+                              stride=stride, padding=padding)
+        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
+
+    def forward(self, x):
+        x = self.proj(x)
+        x = self.norm(x)
+        return x
