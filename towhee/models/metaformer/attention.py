69083685d56f71390d310e937da884d18817cbc8
Junchen He <hovmey@hotmail.com>
2022-09-29 12:40:53 +0800

    add meteformer's specified attention module (#2163)
    Co-authored-by: Junxen <Junxen@users.noreply.github.com>

---
+++ towhee/models/metaformer/attention.py
@@ -0,0 +1,63 @@
+# Built on top of the original implementation at https://github.com/sail-sg/poolformer/blob/main/models/metaformer.py
+#
+# Modifications by Copyright 2022 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from torch import nn
+import torch
+
+
+class Attention(nn.Module):
+    """
+    Attention module that can take tensor with [B, N, C] or [B, C, H, W] as input. Structure specified by metaformer
+    Modified from:
+    https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+    Args:
+        dim (int): dimension
+        head_dim (int): head dimension
+    """
+    def __init__(self, dim, head_dim=32, qkv_bias=False, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        assert dim % head_dim == 0, 'dim should be divisible by head_dim'
+        self.head_dim = head_dim
+        self.num_heads = dim // head_dim
+        self.scale = head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        shape = x.shape
+        if len(shape) == 4:
+            b, c, h, w = shape
+            n = h * w
+            x = torch.flatten(x, start_dim=2).transpose(-2, -1) # (B, N, C)
+        qkv = self.qkv(x).reshape(b, n, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)   # make torchscript happy (cannot use tensor as tuple)
+
+        # trick here to make q@k.t more stable
+        attn = (q * self.scale) @ k.transpose(-2, -1)
+        # attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(b, n, c)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        if len(shape) == 4:
+            x = x.transpose(-2, -1).reshape(b, c, h, w)
+
+        return x
