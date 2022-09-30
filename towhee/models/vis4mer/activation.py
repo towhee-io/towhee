adf193a9dce21bb870a31830a91d38c796d41d69
Junxen <Junxen@users.noreply.github.com>
2022-09-30 11:24:38 +0800

    add activation for vis4mer

---
+++ towhee/models/vis4mer/activation.py
@@ -0,0 +1,43 @@
+# Built on top of the original implementation at https://github.com/md-mohaiminul/ViS4mer
+#
+# Modifications by Copyright 2022 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from torch import nn
+
+
+def Activation(activation=None, dim=-1):
+    """
+    generate activation module
+    Args:
+        activation (str): activation
+        dim (int): dimension
+    return: activation module
+    """
+    if activation in [None, 'id', 'identity', 'linear']:
+        return nn.Identity()
+    elif activation == 'tanh':
+        return nn.Tanh()
+    elif activation == 'relu':
+        return nn.ReLU()
+    elif activation == 'gelu':
+        return nn.GELU()
+    elif activation in ['swish', 'silu']:
+        return nn.SiLU()
+    elif activation == 'glu':
+        return nn.GLU(dim=dim)
+    elif activation == 'sigmoid':
+        return nn.Sigmoid()
+    else:
+        raise NotImplementedError("hidden activation '{}' is not implemented".format(activation))
