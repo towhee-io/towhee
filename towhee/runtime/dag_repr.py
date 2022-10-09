36f6f7ced4243cf8fe9a469a5315393cc15e7d08
shiyu22 <shiyu.chen@zilliz.com>
2022-10-09 15:16:31 +0800

    Add dagrepr ut
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>

--- towhee/runtime/dag_repr.py
+++ towhee/runtime/dag_repr.py
@@ -17,6 +17,12 @@ from towhee.runtime.node_repr import NodeRepr
 
 
 class DAGRepr:
+    """
+    A `DAGRepr` represents a complete DAG.
+
+    Args:
+        dag (`Dict[str, Any]`): The DAG dictionary.
+    """
     def __init__(self, dag: dict):
         self._dag = dag
 
@@ -25,6 +31,24 @@ class DAGRepr:
         return self._dag
 
     def get_nodes(self) -> List:
+        """
+        Get a list of NodeRepr from the DAG.
+
+        Examples:
+        >>> from towhee.runtime.dag_repr import DAGRepr
+        >>> towhee_dag = {
+        ...  '_input': {'inputs': ('a', 'b'), 'outputs': ('a', 'b'), 'fn_type': '_input', 'iteration': 'map'},
+        ...  'e433a': {'function': 'towhee.decode', 'init_args': ('a',), 'init_kws': {'b': 'b'}, 'inputs': 'a', 'outputs': 'c', 'fn_type': 'hub',
+        ...            'iteration': 'map', 'config': None, 'tag': 'main', 'param': None},
+        ...  'b1196': {'function': 'towhee.hub', 'init_args': ('a',), 'init_kws': {'b': 'b'}, 'inputs': ('a', 'b'), 'outputs': 'b', 'fn_type': 'hub',
+        ...            'iteration': 'filter', 'config': None, 'tag': '1.1', 'param': {'filter_columns': 'a'}},
+        ...  '_output': {'inputs': 'd', 'outputs': 'd', 'fn_type': '_output', 'iteration': 'map'},
+        ... }
+        >>> dr = DAGRepr(towhee_dag)
+        >>> nodes = dr.get_nodes()
+        >>> print(nodes[0].name, nodes[2].function, nodes[2].iteration, nodes[2].param, nodes[3].name)
+        _input towhee.clip filter {'filter_columns': 'a'} _output
+        """
         nodes_list = []
         for name in self._dag:
             nodes_list.append(NodeRepr.from_dict(name, self._dag[name]))
