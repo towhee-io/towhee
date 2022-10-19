23847c50588e7b486e6bea6ab6f28269cf436c68
Junchen He <hovmey@hotmail.com>
2022-10-19 16:17:24 +0800

    add ut for rank correction (#2208)
    Co-authored-by: Junxen <Junxen@users.noreply.github.com>

---
+++ tests/unittests/models/vis4mer/test_rank_correction.py
@@ -0,0 +1,27 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from towhee.models.vis4mer.utils import rank_correction
+
+
+class TransitionTest(unittest.TestCase):
+
+    def test_model(self):
+        p = rank_correction(measure='legs', n=2)
+        self.assertEqual(p.shape, (1, 2))
+
+
+if __name__ == '__main__':
+    unittest.main()
