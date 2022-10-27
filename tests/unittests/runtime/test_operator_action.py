b3d22894cce39b5d8406c74670aa55e8cbeca1f1
shiyu22 <shiyu.chen@zilliz.com>
2022-10-27 15:01:50 +0800

    Add Pipeline and OpAction
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    Update ut
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    Update UT
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>

---
+++ tests/unittests/runtime/test_operator_action.py
@@ -0,0 +1,55 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from typing import Callable
+
+from towhee.runtime.operator_manager.operator_action import OperatorAction
+
+
+def f():
+    pass
+
+
+# pylint: disable=protected-access
+class TestOperatorAction(unittest.TestCase):
+    """
+    Test OperatorAction
+    """
+
+    def test_hub(self):
+        op_action = OperatorAction.from_hub('test/image-decode', (), {})
+        op_info = {
+            'operator': 'test/image-decode',
+            'type': 'hub',
+            'init_args': None,
+            'init_kws': None,
+            'tag': 'main'
+        }
+        self.assertEqual(op_action.serialize(), op_info)
+
+    # TODO: Support lambda and callable with more test
+    def test_lambda(self):
+        op_action = OperatorAction.from_lambda(lambda x: x+1)
+        self.assertEqual(op_action._type, 'lambda')
+        self.assertIsInstance(op_action._loaded_fn, Callable)
+        with self.assertRaises(ValueError):
+            op_action.serialize()
+
+    def test_callable(self):
+        op_action = OperatorAction.from_callable(f)
+        self.assertEqual(op_action._type, 'callable')
+        self.assertIsInstance(op_action._loaded_fn, Callable)
+        with self.assertRaises(ValueError):
+            op_action.serialize()
---
+++ towhee/runtime/operator_manager/operator_action.py
@@ -0,0 +1,97 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=protected-access
+class OperatorAction:
+    """
+    Action wrapper.
+
+    Different types of operations are loaded into this wrapper for the DAG, each
+    operators can be loaded and run in different ways.
+    """
+    def __init__(self):
+        self._loaded_fn = None
+        self._type = None
+        self._tag = 'main'
+
+    @property
+    def type(self):
+        return self._type
+
+    @staticmethod
+    def from_hub(name, args, kwargs):
+        """Create an Action for hub op.
+
+        Args:
+            name (str): The op name or the path to an op.
+            args (list): The op args.
+            kwargs (dict): The op kwargs.
+
+        Returns:
+            Action: The action.
+        """
+        action = OperatorAction()
+        action._op_name = name
+        action._op_args = args
+        action._op_kwargs = kwargs
+        action._type = 'hub'
+        return action
+
+    # TODO: Deal with serialized input vs non_serialized
+    @staticmethod
+    def from_lambda(fn):
+        """Create an Action for lambda op.
+
+        Args:
+            fn (lamda): The lambda function for op.
+
+        Returns:
+            Action: The action.
+        """
+        action = OperatorAction()
+        action._fn = fn
+        action._loaded_fn = fn
+        action._type = 'lambda'
+        return action
+
+    # TODO: Deal with serialized input vs non_serialized
+    @staticmethod
+    def from_callable(fn):
+        """Create an Action for callable op.
+
+        Args:
+            fn (callable): The callable function for op.
+
+        Returns:
+            Action: The action.
+        """
+        action = OperatorAction()
+        action._fn = fn
+        action._loaded_fn = fn
+        action._type = 'callable'
+        return action
+
+    def serialize(self):
+        if self._type == 'hub':
+            return {
+                'operator': self._op_name,
+                'type': self._type,
+                'init_args': self._op_args if len(self._op_args) != 0 else None,
+                'init_kws': self._op_kwargs if len(self._op_kwargs) != 0 else None,
+                'tag': self._tag
+            }
+        elif self._type == 'lambda':
+            raise ValueError('Lambda not supported yet.')
+        elif self._type == 'callable':
+            raise ValueError('Callable not supported yet.')
