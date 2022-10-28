aa025e66d1eafa6b8992266ff529eab831c278ce
junjiejiangjjj <junjie.jiang@zilliz.com>
2022-10-28 14:03:33 +0800

    Add pipeline test (#2232)
    * Add pipeline test
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>
    
    * rm useless op cache path
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>
    
    * dq default size 1000
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>
    
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>

--- towhee/runtime/operator_manager/operator_loader.py
+++ towhee/runtime/operator_manager/operator_loader.py
@@ -22,9 +22,7 @@ from pkg_resources import DistributionNotFound
 
 from towhee.operator import Operator
 from towhee.operator.nop import NOPNodeOperator
-from towhee.operator.concat_operator import ConcatOperator
 from towhee.hub.file_manager import FileManager
-from towhee.engine import LOCAL_OPERATOR_CACHE
 from .operator_registry import OperatorRegistry
 
 
@@ -38,17 +36,10 @@ class OperatorLoader:
             Local cache path to use. If not specified, it will default to
             `$HOME/.towhee/operators`.
     """
-    def __init__(self, cache_path: str = None):
-        if cache_path is None:
-            self._cache_path = LOCAL_OPERATOR_CACHE
-        else:
-            self._cache_path = Path(cache_path)
 
-    def _load_interal_op(self, op_name: str, arg: List[Any], kws: Dict[str, Any]):
+    def _load_interal_op(self, op_name: str, arg: List[Any], kws: Dict[str, Any]): # pylint: disable=unused-argument
         if op_name in ['_input', '_output']:
             return NOPNodeOperator()
-        elif op_name == '_concat':
-            return ConcatOperator(*arg, **kws)
         else:
             # Not a interal operator
             return None
