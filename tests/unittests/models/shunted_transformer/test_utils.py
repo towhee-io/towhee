444e7a1c7905a0e673c6e37157d6440e21285468
Jael Gu <mengjia.gu@zilliz.com>
2022-11-01 14:05:31 +0800

    Add modified Mlp for shunted transformer (#2246)
    Signed-off-by: Jael Gu <mengjia.gu@zilliz.com>
    
    Signed-off-by: Jael Gu <mengjia.gu@zilliz.com>

--- tests/unittests/models/shunted_transformer/test_utils.py
+++ tests/unittests/models/shunted_transformer/test_utils.py
@@ -16,17 +16,27 @@
 import unittest
 import torch
 
-from towhee.models.shunted_transformer import OverlapPatchEmbed
+from towhee.models.shunted_transformer import OverlapPatchEmbed, Mlp
 
 
 class TestUtils(unittest.TestCase):
+    """
+    Test utils for Shunted Transformer
+    """
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
     def test_patch_embed2d(self):
         test_shape = (1, 3, 4, 4)
-        fake_img = torch.rand(test_shape)
-        model = OverlapPatchEmbed(img_size=4, patch_size=2, stride=1, embed_dim=5)
+        fake_img = torch.rand(test_shape).to(self.device)
+        model = OverlapPatchEmbed(img_size=test_shape[-1], patch_size=2, stride=1, embed_dim=5).to(self.device)
         out = model(fake_img)
-        print(out.shape)
         self.assertTrue(out.shape == (1, 25, 5))
+
+    def test_mlp(self):
+        dummy_input = torch.rand(1, 16, 3).to(self.device)
+        model = Mlp(in_features=3, hidden_features=5, out_features=4).to(self.device)
+        out = model(dummy_input, 4, 4)
+        self.assertTrue(out.shape == (1, 16, 4))
 
 
 if __name__ == '__main__':
