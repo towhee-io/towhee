f914a612dd9832175593744c5b7c504774284421
shiyu22 <shiyu.chen@zilliz.com>
2022-12-14 11:07:21 +0800

    Add triton client (#2333)
    * Add triton client
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    Add ut
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    Add json ut
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    Update np_format
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    Add batch api
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    Add examples in docstring
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    Move httpclient
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    * Update ut with diff args
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>

---
+++ tests/unittests/triton/test_pipeline_client.py
@@ -0,0 +1,146 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import json
+import struct
+import numpy as np
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from towhee.dc2 import pipe
+from towhee.serve.triton.pipeline_client import Client
+from towhee.utils.thirdparty.dail_util import dill as pickle
+from towhee.utils.np_format import NumpyArrayDecoder, NumpyArrayEncoder
+import towhee.serve.triton.bls.pipeline_model as pipe_model
+from towhee.serve.triton.bls.python_backend_wrapper import pb_utils
+
+# pylint:disable=protected-access
+# pylint:disable=inconsistent-quotes
+# pylint:disable=unused-argument
+
+
+def deserialize_bytes_tensor(encoded_tensor):
+    strs = []
+    offset = 0
+    val_buf = encoded_tensor
+    while offset < len(val_buf):
+        l = struct.unpack_from("<I", val_buf, offset)[0]
+        offset += 4
+        sb = struct.unpack_from("<{}s".format(l), val_buf, offset)[0]
+        offset += l
+        strs.append(sb)
+    return np.array(strs, dtype=np.object_)
+
+
+class MockHttpClient:
+    """
+    mock http client
+    """
+    def __init__(self, url):
+        self._url = url
+        self._pipe = None
+
+    def set_pipeline(self, pipeline):
+        with TemporaryDirectory(dir='./') as root:
+            pf = Path(root) / 'pipe.pickle'
+            with open(pf, 'wb') as f:
+                pickle.dump(pipeline.dag_repr, f)
+
+            self._pipe = pipe_model.TritonPythonModel()
+            self._pipe._load_pipeline(pf)
+            self._pipe.pipe.__call__ == self._pipe.pipe.batch  # pylint:disable=pointless-statement
+
+    def infer(self, model_name, inputs):
+        in_data_list = deserialize_bytes_tensor(inputs[0]._raw_data)
+        json_data = []
+        for in_data in in_data_list:
+            input_data = json.loads(in_data, cls=NumpyArrayDecoder)
+            json_data.append([json.dumps(input_data, cls=NumpyArrayEncoder)])
+        triton_input = np.array(json_data, dtype=np.object_)
+        inputs = pb_utils.InferenceRequest([pb_utils.Tensor('INPUT0', triton_input)], [], model_name)
+        res = self._pipe.execute([inputs])
+        return MockRes(res)
+
+    def close(self):
+        pass
+
+
+class MockRes:
+    """
+    mock response class for triton client
+    """
+    def __init__(self, outputs):
+        self._outputs = outputs[0]
+
+    def as_numpy(self, name):
+        res = self._outputs.output_tensors()[0].as_numpy()
+        return res
+
+
+class TestTritonClient(unittest.TestCase):
+    """
+    Unit test for triton client.
+    """
+    def test_single_params(self):
+        p = (
+            pipe.input('num')
+            .map('num', 'arr', lambda x: x*10)
+            .map(('num', 'arr'), 'ret', lambda x, y: x + y)
+            .output('ret')
+        )
+
+        url = '127.0.0.1:8000'
+        with Client(url) as client:
+            client._client = MockHttpClient(url)
+            client._client.set_pipeline(p)
+            res1 = client(1)
+            self.assertEqual(len(res1), 1)
+            expect1 = [[11]]
+            for index, item in enumerate(res1[0], 0):
+                self.assertEqual(item, expect1[index])
+
+            res2 = client.batch([1, 2, 3])
+            self.assertEqual(len(res2), 3)
+            expect2 = [[11], [22], [33]]
+            for index, single in enumerate(res2):
+                for item in single:
+                    self.assertEqual(item, expect2[index])
+
+    def test_multi_params(self):
+        p = (
+            pipe.input('nums', 'arr')
+            .flat_map('nums', 'num', lambda x: x)
+            .map(('num', 'arr'), 'ret', lambda x, y: x + y)
+            .output('ret')
+        )
+
+        url = '127.0.0.1:8000'
+        with Client(url) as client:
+            in0 = [1, 2, 3]
+            in1 = np.random.rand(3, 3)
+            client._client = MockHttpClient(url)
+            client._client.set_pipeline(p)
+            res1 = client(in0, in1)
+            self.assertEqual(len(res1), 1)
+            self.assertEqual(len(res1[0]), 3)
+            for index, item in enumerate(res1[0], 1):
+                self.assertTrue((item[0] == (in1 + index)).all())
+
+            # test with batch
+            res2 = client.batch([[in0, in1], [in0, in1], [in0, in1], [in0, in1]])
+            self.assertEqual(len(res2), 4)
+            for single in res2:
+                for index, item in enumerate(single, 1):
+                    self.assertTrue((item[0] == (in1 + index)).all())
---
+++ towhee/serve/triton/pipeline_client.py
@@ -0,0 +1,82 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json
+import numpy as np
+
+from towhee.utils.np_format import NumpyArrayDecoder, NumpyArrayEncoder
+from towhee.serve.triton.constant import PIPELINE_NAME
+
+
+class Client:
+    """
+    Triton HttpClient
+
+    Args:
+        url(`string`): IP address and port for the triton server, such as '<host>:<port>' and  '127.0.0.1:8001'.
+        model_name(`string`): Model name in triton, defaults to 'pipeline'.
+
+    Examples:
+        >>> from towhee import triton_client
+        >>> client = triton_client('<your-ip>:<your-port>')
+        >>> res = client('your-data')
+        >>> client.close()
+
+        You can also run as following:
+        >>> with triton_client('<your-ip>:<your-port>') as client:
+        ...     res = client('your-data')
+    """
+    def __init__(self, url: str, model_name: str = PIPELINE_NAME):
+        from towhee.utils.tritonclient_utils import httpclient  # pylint: disable=import-outside-toplevel
+        self._client = httpclient.InferenceServerClient(url)
+        self._model_name = model_name
+
+    def __call__(self, *args):
+        if len(args) > 1:
+            args = [args]
+        inputs = self._solve_inputs(args)
+        response = self._client.infer(self._model_name, inputs)
+        outputs = self._solve_responses(response)
+        return outputs
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):  # pylint: disable=redefined-builtin
+        self.close()
+
+    def batch(self, args_list):
+        inputs = self._solve_inputs(args_list, len(args_list))
+        response = self._client.infer(self._model_name, inputs)
+        outputs = self._solve_responses(response)
+        return outputs
+
+    def close(self):
+        self._client.close()
+
+    @staticmethod
+    def _solve_inputs(args, size=1):
+        from towhee.utils.tritonclient_utils import httpclient  # pylint: disable=import-outside-toplevel
+        inputs = [httpclient.InferInput('INPUT0', [size, 1], 'BYTES')]
+        arg_list = []
+        for arg in args:
+            arg_list.append([json.dumps(arg, cls=NumpyArrayEncoder)])
+        data = np.array(arg_list, dtype=np.object_)
+        inputs[0].set_data_from_numpy(data)
+        return inputs
+
+    @staticmethod
+    def _solve_responses(response):
+        res = response.as_numpy('OUTPUT0')[0]
+        outputs = json.loads(res, cls=NumpyArrayDecoder)
+        return outputs
