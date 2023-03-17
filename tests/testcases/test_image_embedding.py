dcc54de4754c18b948cb7ed355201ae6e262ff1c
NicoYuan1986 <109071306+NicoYuan1986@users.noreply.github.com>
2023-03-17 15:05:53 +0800

    Delete dc2 in test cases (#2467)
    Signed-off-by: nico <cheng.yuan@zilliz.com>

--- tests/testcases/test_image_embedding.py
+++ tests/testcases/test_image_embedding.py
@@ -4,58 +4,99 @@ sys.path.append("../../tests")
 import time
 import threading
 import numpy as np 
-from towhee import pipeline
+from towhee import AutoPipes, AutoConfig
 from common import common_func as cf
 
 data_path = "images/"
+pipeline_name = 'text_image_embedding'
+
 
 class TestImageEmbeddingInvalid:
-    """ Test case of invalid embedding interface """
+    """ Test case of invalid text_image_embedding interface """
 
-    def test_embedding_no_parameter(self, pipeline_name):
+    def test_embedding_config_no_parameter(self):
         """
         target: test embedding for invalid scenario
-        method:  embedding with no image, now issue 154
+        method:  embedding with no config parameter
         expected: raise exception
         """
-        embedding_pipeline = pipeline(pipeline_name)
         try:
-            embedding = embedding_pipeline()
+            AutoConfig.load_config()
+        except TypeError as e:
+            print("Raise Exception: missing 1 required positional argument: 'name'")
+            assert 'missing' in str(e)
+        return True
+
+    def test_embedding_pipeline_no_parameter(self):
+        """
+        target: test embedding for invalid scenario
+        method:  embedding with no pipeline parameter
+        expected: raise exception
+        """
+        try:
+            AutoPipes.pipeline()
+        except TypeError as e:
+            print("Raise Exception: missing 1 required positional argument: 'name'")
+            assert 'missing' in str(e)
+        return True
+
+    def test_embedding_wrong_pipeline_name(self):
+        """
+        target: test embedding for invalid scenario
+        method:  embedding with wrong pipeline name
+        expected: raise exception
+        """
+        try:
+            AutoConfig.load_config([0])
+        except TypeError as e:
+            print("Raise Exception: unhashable type")
+            assert 'unhashable type' in str(e)
+        return True
+
+    def test_embedding_nonexistent_pipeline_name(self):
+        """
+        target: test embedding for invalid scenario
+        method:  embedding with nonexistent pipeline name
+        expected: raise exception
+        """
+        try:
+            AutoConfig.load_config(' ')
+        except TypeError as e:
+            print("Raise Exception: Can not find config")
+        return True
+
+    def test_embedding_nonexistent_model(self):
+        """
+        target: test embedding for invalid scenario
+        method:  embedding with nonexistent pipeline model
+        expected: raise exception
+        """
+        emb_conf = AutoConfig.load_config(pipeline_name)
+        try:
+            emb_conf.model = 'clip1000'
+            AutoPipes.pipeline(pipeline_name, emb_conf)
         except RuntimeError as e:
-            print("Raise Exception: %s" % e)
-
+            assert 'failed' in str(e)
+        except KeyError as k:
+            assert 'clip1000' in str(k)
         return True
 
-    def test_embedding_wrong_parameter(self, pipeline_name):
+    def test_embedding_nonexistent_image(self):
         """
         target: test embedding for invalid scenario
-        method:  embedding with no image, now issue 154
+        method:  embedding with nonexistent image
         expected: raise exception
         """
-        embedding_pipeline = pipeline(pipeline_name)
-        try:
-            embedding = embedding_pipeline("")
-        except Exception as e:
-            print("Raise Exception: %s" % e)
-
-        return True
-
-    def test_embedding_not_exist_image(self, pipeline_name):
-        """
-        target: test embedding for invalid scenario
-        method:  embedding with no image, now issue 154
-        expected: raise exception
-        """
-        embedding_pipeline = pipeline(pipeline_name)
+        emb_pipe = AutoPipes.pipeline(pipeline_name)
         not_exist_img = "towhee_no_exist.jpg"
         try:
-            embedding = embedding_pipeline(not_exist_img)
-        except Exception as e:
-            print("Raise Exception: %s" % e)
-
+            emb_pipe(not_exist_img).to_list()
+        except RuntimeError as e:
+            print("Raise Exception: Read image towhee_no_exist.jpg failed")
+            assert "failed" in str(e)
         return True
 
-    def test_embedding_image_list(self, pipeline_name):
+    def test_embedding_image_list(self):
         """
         target: test embedding with image list
         method:  embedding with image list
@@ -66,44 +107,48 @@ class TestImageEmbeddingInvalid:
         img = cf.create_image()
         for i in range(N):
             img_list.append(img)
-        embedding_pipeline = pipeline(pipeline_name)
+        emb_pipe = AutoPipes.pipeline(pipeline_name)
         try:
-            embedding = embedding_pipeline(img_list)
-        except Exception as e:
-            print("Raise Exception: %s" % e)
-
+            emb_pipe(img_list).to_list()
+        except AttributeError as e:
+            assert "list" in str(e)
+        except RuntimeError as r:
+            assert "list" in str(r)
         return True
+
 
 class TestImageEmbeddingValid:
     """ Test case of valid embedding interface """
 
-    def test_embedding_one_image(self, pipeline_name, embedding_size):
+    def test_embedding_one_image(self, pipeline_model, embedding_size):
         """
         target: test embedding for normal case
         method:  embedding with one image
         expected: return embeddings
         """
-        embedding_pipeline = pipeline(pipeline_name)
-        embedding = embedding_pipeline(data_path + "towhee_test_image0.jpg")
-        assert embedding.size == embedding_size
-
+        emb_conf = AutoConfig.load_config(pipeline_name)
+        emb_conf.model = pipeline_model
+        emb_pipe = AutoPipes.pipeline(pipeline_name, emb_conf)
+        res = emb_pipe(data_path + "towhee_test_image0.jpg").to_list()[0][0].tolist()
+        assert len(res) == embedding_size
         return True
 
-    def test_embedding_same_images(self, pipeline_name, embedding_size):
+    def test_embedding_same_images(self, pipeline_model, embedding_size):
         """
         target: test embedding for normal case
         method:  embedding with same images
         expected: return identical embeddings
         """
-        embedding_pipeline = pipeline(pipeline_name)
-        embedding = embedding_pipeline(data_path + "towhee_test_image0.jpg")
-        embedding1 = embedding_pipeline(data_path + "towhee_test_image2.jpg")
-        assert embedding.size == embedding_size
-        assert embedding.all() == embedding1.all()
-
+        emb_conf = AutoConfig.load_config(pipeline_name)
+        emb_conf.model = pipeline_model
+        emb_pipe = AutoPipes.pipeline(pipeline_name, emb_conf)
+        res1 = emb_pipe(data_path + "towhee_test_image0.jpg").to_list()[0][0].tolist()
+        res2 = emb_pipe(data_path + "towhee_test_image2.jpg").to_list()[0][0].tolist()
+        assert len(res1) == embedding_size
+        assert res1 == res2
         return True
 
-    def test_embedding_different_images(self, pipeline_name, embedding_size):
+    def test_embedding_different_images(self, pipeline_model, embedding_size):
         """
         target: test embedding for normal case
         method:  embedding with different images
@@ -111,34 +156,35 @@ class TestImageEmbeddingValid:
         """
         nums = 2
         embeddings = []
-        embedding_pipeline = pipeline(pipeline_name)
+        emb_conf = AutoConfig.load_config(pipeline_name)
+        emb_conf.model = pipeline_model
+        emb_pipe = AutoPipes.pipeline(pipeline_name, emb_conf)
         for i in range(nums):
-            embedding = embedding_pipeline(data_path + "towhee_test_image%d.jpg" % i)
+            embedding = emb_pipe(data_path + "towhee_test_image%d.jpg" % i).to_list()[0][0].tolist()
             embeddings.append(embedding)
-            assert embedding.size == embedding_size
-
+            assert len(embedding) == embedding_size
         assert embeddings[0] is not embeddings[1]
-
         return True
 
-    def test_embedding_one_image_multiple_times(self, pipeline_name, embedding_size):
+    def test_embedding_one_image_multiple_times(self, pipeline_model, embedding_size):
         """
         target: test embedding for normal case
         method: embedding one image for multiple times
         expected: return identical embeddings
         """
         nums = 10
-        embedding_pipeline = pipeline(pipeline_name)
-        embedding_last = embedding_pipeline(data_path + "towhee_test_image1.jpg")
+        emb_conf = AutoConfig.load_config(pipeline_name)
+        emb_conf.model = pipeline_model
+        emb_pipe = AutoPipes.pipeline(pipeline_name, emb_conf)
+        embedding_last = emb_pipe(data_path + "towhee_test_image1.jpg").to_list()[0][0].tolist()
         for i in range(nums):
-            embedding = embedding_pipeline(data_path + "towhee_test_image1.jpg")
-            assert embedding.size == embedding_size
-            assert embedding.all() == embedding_last.all()
+            embedding = emb_pipe(data_path + "towhee_test_image1.jpg").to_list()[0][0].tolist()
+            assert len(embedding) == embedding_size
+            assert embedding == embedding_last
             print("embedding images for %d round" % (i+1))
-
         return True
 
-    def test_embedding_images_multiple_times(self, pipeline_name, embedding_size):
+    def test_embedding_images_multiple_times(self, pipeline_model, embedding_size):
         """
         target: test embedding for normal case
         method:  embedding images for multiple times
@@ -146,19 +192,21 @@ class TestImageEmbeddingValid:
         """
         nums = 2
         times = 10
-        embedding_pipeline = pipeline(pipeline_name)
+        emb_conf = AutoConfig.load_config(pipeline_name)
+        emb_conf.model = pipeline_model
+        emb_pipe = AutoPipes.pipeline(pipeline_name, emb_conf)
         for i in range(times):
             embeddings = []
             for j in range(nums):
-                embedding = embedding_pipeline(data_path + "towhee_test_image%d.jpg" % j)
+                embedding = emb_pipe(data_path + "towhee_test_image%d.jpg" % j).to_list()[0][0].tolist()
                 embeddings.append(embedding)
-                assert embedding.size == embedding_size
+                assert len(embedding) == embedding_size
             assert embeddings[0] is not embeddings[1]
             print("embedding images for %d round" % (i+1))
 
         return True
 
-    def test_embedding_concurrent_multi_threads(self, pipeline_name, embedding_size):
+    def test_embedding_concurrent_multi_threads(self, pipeline_model, embedding_size):
         """
         target: test embedding for normal case
         method:  embedding with concurrent multi-processes
@@ -166,11 +214,13 @@ class TestImageEmbeddingValid:
         """
         threads_num = 10
         threads = []
+        emb_conf = AutoConfig.load_config(pipeline_name)
+        emb_conf.model = pipeline_model
+        emb_pipe = AutoPipes.pipeline(pipeline_name, emb_conf)
 
         def embedding():
-            embedding_pipeline = pipeline(pipeline_name)
-            embedding = embedding_pipeline(data_path + "towhee_test_image2.jpg")
-            assert embedding.size == embedding_size
+            embeddings = emb_pipe(data_path + "towhee_test_image2.jpg").to_list()[0][0].tolist()
+            assert len(embeddings) == embedding_size
 
         for i in range(threads_num):
             t = threading.Thread(target=embedding)
@@ -179,60 +229,59 @@ class TestImageEmbeddingValid:
             time.sleep(0.2)
         for t in threads:
             t.join()
-
         return True
 
 
 class TestImageEmbeddingStress:
     """ Test case of stress """
 
-    def test_embedding_more_times(self, pipeline_name, embedding_size):
-       """
+    def test_embedding_more_times(self, pipeline_model, embedding_size):
+        """
         target: test embedding for stress scenario
         method: embedding for N times
         expected: return embeddings
-       """      
-       embedding_pipeline = pipeline(pipeline_name)
-       nums = 1000
-       for i in range(nums):
+        """
+        emb_conf = AutoConfig.load_config(pipeline_name)
+        emb_conf.model = pipeline_model
+        emb_pipe = AutoPipes.pipeline(pipeline_name, emb_conf)
+        nums = 1000
+        for i in range(nums):
             try:
-                embedding = embedding_pipeline(data_path + "towhee_test_image0.jpg")
-                assert embedding.size == embedding_size
+                embedding = emb_pipe(data_path + "towhee_test_image0.jpg").to_list()[0][0].tolist()
+                assert len(embedding) == embedding_size
             except Exception as e:
                 print("Raise Exception: %s" % e)
             print("embedding images for %d round" % (i+1))
+        return True
 
-       return True
 
 class TestImageEmbeddingPerformance:
     """ Test case of performance """
 
-    def test_embedding_avg_time(self, pipeline_name, embedding_size):
+    def test_embedding_avg_time(self, pipeline_model, embedding_size):
         """
         target: test embedding for performance scenario
         method: embedding N times and calculate the average embedding time 
         expected: return embeddings
         """
-        embedding_pipeline = pipeline(pipeline_name)
-        avg_time = 0
+        emb_conf = AutoConfig.load_config(pipeline_name)
+        emb_conf.model = pipeline_model
+        emb_pipe = AutoPipes.pipeline(pipeline_name, emb_conf)
         time_cost = []
         num = 10
-        for i in range (num):
+        for i in range(num):
             try:
                 time_start = time.time()
-                embedding = embedding_pipeline(data_path + "towhee_test_image0.jpg")
+                embedding = emb_pipe(data_path + "towhee_test_image0.jpg").to_list()[0][0].tolist()
                 time_cost.append(time.time() - time_start)
-                assert embedding.size == embedding_size
+                assert len(embedding) == embedding_size
             except Exception as e:
-                print( "Raise Exception: %s" % e)
+                print("Raise Exception: %s" % e)
             print("embedding images for %d round" % (i+1))
         time_cost = np.array(time_cost)
         total_time = np.sum(time_cost)
         print(f"The total time is", total_time)
         avg_time = round(total_time/num, 3)
         print(f"The average time is", avg_time)
-
         
         return True
-
-
--- tests/testcases/test_main.py
+++ tests/testcases/test_main.py
@@ -1,10 +1,11 @@
 # coding : UTF-8
 from operator import methodcaller
 from test_image_embedding import *
-from test_pipeline import *
-from test_audio_embedding import *
+# from test_pipeline import *
+# from test_audio_embedding import *
 from test_data_collection import *
 from test_command import *
+
 
 def pipeline_register():
 
@@ -16,6 +17,7 @@ def pipeline_register():
 
     return pipeline_names
 
+
 def pipeline_runner():
 
     invalid_pipeline_obj = TestPipelineInvalid()
@@ -23,7 +25,7 @@ def pipeline_runner():
         if not func.startswith("__"):
             print("Testing %s" % func)
             res = methodcaller(func)(invalid_pipeline_obj)
-            if res == None:
+            if res is None:
                 print("%s PASS" % func)
             else:
                 print("%s FAIL" % func)
@@ -35,11 +37,12 @@ def pipeline_runner():
             if not func.startswith("__"):
                 print("Testing %s:%s" % (func, pipeline_name))
                 res = methodcaller(func, pipeline_name)(valid_pipeline_obj)
-                if res == None:
+                if res is None:
                     print("%s:%s PASS" % (func, pipeline_name))
                 else:
                     print("%s:%s FAIL" % (func, pipeline_name))
     return True
+
 
 def image_class_pipeline_register():
     # skip efficientnetb7 image pipeline for memory shortage
@@ -49,70 +52,73 @@ def image_class_pipeline_register():
     #                   "towhee/image-embedding-swinlarge", "towhee/image-embedding-vitlarge"]
     # embedding_sizes = [2048, 2048, 2560, 2048, 2048, 1024, 1536, 1024]
 
-    pipeline_names = ["image-embedding", "towhee/image-embedding-efficientnetb5",
-                      "towhee/image-embedding-resnet101", "towhee/image-embedding-resnet50",
-                      "towhee/image-embedding-swinbase", "towhee/image-embedding-swinlarge"]
+    pipeline_models = ["clip_vit_base_patch16", "clip_vit_base_patch32",
+                       "clip_vit_large_patch14", "clip_vit_large_patch14_336",
+                       "blip_itm_base_coco", "blip_itm_base_flickr",
+                       "blip_itm_large_coco", "blip_itm_large_flickr"]
 
-    embedding_sizes = [2048, 2048, 2048, 2048, 1024, 1536]
+    embedding_sizes = [512, 512, 768, 768, 256, 256, 256, 256]
 
     # skip multiple threads tests for memory shortage
     skipped_cases = ["test_embedding_concurrent_multi_threads", "test_embedding_more_times", "test_embedding_avg_time"]
- 
+    pipeline_models = pipeline_models[:2]
+    embedding_sizes = embedding_sizes[:2]
+    return pipeline_models, embedding_sizes, skipped_cases
 
-    return pipeline_names, embedding_sizes, skipped_cases
 
 def image_class_pipeline_runner():
 
-    pipeline_names, embedding_sizes, skipped_cases = image_class_pipeline_register()
-    for (pipeline_name, embedding_size_each) in zip(pipeline_names, embedding_sizes):
+    pipeline_models, embedding_sizes, skipped_cases = image_class_pipeline_register()
+    for (pipeline_model, embedding_size_each) in zip(pipeline_models, embedding_sizes):
         invalid_embedding_obj = TestImageEmbeddingInvalid()
         for func in dir(TestImageEmbeddingInvalid):
             if func in skipped_cases:
                 continue
             if not func.startswith("__"):
-                print("Testing %s:%s" % (func, pipeline_name))
-                res = methodcaller(func, pipeline_name)(invalid_embedding_obj)
+                print("Testing %s" % func)
+                res = methodcaller(func)(invalid_embedding_obj)
                 if res == 1:
-                    print("%s:%s PASS" % (func, pipeline_name))
+                    print("%s PASS" % func)
                 else:
-                    print("%s:%s FAIL" % (func, pipeline_name))
+                    print("%s FAIL" % func)
         valid_embedding_obj = TestImageEmbeddingValid()
         for func in dir(TestImageEmbeddingValid):
             if func in skipped_cases:
                 continue
             if not func.startswith("__"):
-                print("Testing %s:%s" % (func, pipeline_name))
-                res = methodcaller(func, pipeline_name, embedding_size_each)(valid_embedding_obj)
+                print("Testing %s:%s" % (func, pipeline_model))
+                res = methodcaller(func, pipeline_model, embedding_size_each)(valid_embedding_obj)
                 if res == 1:
-                    print("%s:%s PASS" % (func, pipeline_name))
+                    print("%s:%s PASS" % (func, pipeline_model))
                 else:
-                    print("%s:%s FAIL" % (func, pipeline_name))
+                    print("%s:%s FAIL" % (func, pipeline_model))
         
         test_valid_embedding = TestImageEmbeddingStress()
         for func in dir(TestImageEmbeddingStress):
             if func in skipped_cases:
                 continue
             if not func.startswith("__"):
-                print("Testing %s:%s" % (func, pipeline_name))
-                res = methodcaller(func, pipeline_name, embedding_size_each)(test_valid_embedding)
+                print("Testing %s:%s" % (func, pipeline_model))
+                res = methodcaller(func, pipeline_model, embedding_size_each)(test_valid_embedding)
                 if res == 1:
-                    print("%s:%s PASS" % (func, pipeline_name))
+                    print("%s:%s PASS" % (func, pipeline_model))
                 else:
-                    print("%s:%s FAIL" % (func, pipeline_name))
+                    print("%s:%s FAIL" % (func, pipeline_model))
 
         test_valid_embedding_per = TestImageEmbeddingPerformance()
         for func in dir(TestImageEmbeddingPerformance):
             if func in skipped_cases:
                 continue
             if not func.startswith("__"):
-                print("Testing %s:%s" % (func, pipeline_name))
-                res = methodcaller(func, pipeline_name, embedding_size_each)(test_valid_embedding_per)
+                print("Testing %s:%s" % (func, pipeline_model))
+                res = methodcaller(func, pipeline_model, embedding_size_each)(test_valid_embedding_per)
                 if res == 1:
-                    print("%s:%s PASS" % (func, pipeline_name))
+                    print("%s:%s PASS" % (func, pipeline_model))
                 else:
-                    print("%s:%s FAIL" % (func, pipeline_name))
+                    print("%s:%s FAIL" % (func, pipeline_model))
 
     return True
+
 
 def audio_class_pipeline_register():
 
@@ -128,6 +134,7 @@ def audio_class_pipeline_register():
 
 
     return pipeline_names, embedding_sizes, skipped_cases
+
 
 def audio_class_pipeline_runner():
 
@@ -182,6 +189,7 @@ def audio_class_pipeline_runner():
 
     return True
 
+
 def data_collection_API_register():
 
     data_colection_API_names = ["stream", "unstream", "map", "filter", "zip", "batch", "rolling", 
@@ -195,7 +203,7 @@ def data_collection_API_register():
 
 
 def data_collection_API_cases_runner():
-
+    """"
     data_colection_API_names, skipped_API = data_collection_API_register()
     for data_colection_API_name in data_colection_API_names:
         invalid_data_collection_API_obj = TestDataCollectionAPIsInvalid()
@@ -209,11 +217,10 @@ def data_collection_API_cases_runner():
                     print("%s:%s PASS" % (func, data_colection_API_name))
                 else:
                     print("%s:%s FAIL" % (func, data_colection_API_name))
+    """
 
     data_collection_API_obj = TestDataCollectionAPIsValid()
     for func in dir(data_collection_API_obj):
-        if func in skipped_API:
-            continue
         if not func.startswith("__"):
             print("Testing %s" % func)
             res = methodcaller(func)(data_collection_API_obj)
@@ -227,6 +234,10 @@ def data_collection_API_cases_runner():
         if not func.startswith("__"):
             print("Testing %s" % func)
             res = methodcaller(func)(invalid_pipe_API_obj)
+            if res == 1:
+                print("%s PASS" % func)
+            else:
+                print("%s FAIL" % func)
 
 
 def command_cases_runner():
@@ -242,13 +253,13 @@ def command_cases_runner():
 
 
 def test_caller():
-
-    pipeline_runner()
+    # The usage of pipeline has been deleted, the test of new function 'pipe' is in 'test_data_collection.py'
+    # pipeline_runner()
     image_class_pipeline_runner()
     data_collection_API_cases_runner()
     # skip audio tests for issue 463
     # audio_class_pipeline_runner()
-    command_cases_runner()
+    # command_cases_runner() S3 has been removed
 
     return True
 
