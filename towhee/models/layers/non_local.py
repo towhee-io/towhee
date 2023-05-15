a196f11d9a677ee6c981a54e170f63abb828c46a
ChengZi <chen.zhang@zilliz.com>
2023-05-15 20:31:19 +0800

    add license (#2535)
    Signed-off-by: ChengZi <chen.zhang@zilliz.com>

--- towhee/models/layers/non_local.py
+++ towhee/models/layers/non_local.py
@@ -1,7 +1,19 @@
 # Non-local block using embedded gaussian
 # Code from
 # https://github.com/AlexHex7/Non-local_pytorch/blob/master/Non-Local_pytorch_0.3.1/lib/non_local_embedded_gaussian.py
-# modified by Zilliz.
+# Modifications by Copyright 2022 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import torch
 from torch import nn
