8ff04bf870e8f16708523c73c0cdc782810e5dff
Kaiyuan Hu <kaiyuan.hu@zilliz.com>
2023-05-23 15:33:21 +0800

    Add docstring for triton functions and update docs
    Signed-off-by: Kaiyuan Hu <kaiyuan.hu@zilliz.com>

--- towhee/serve/server_builder.py
+++ towhee/serve/server_builder.py
@@ -21,10 +21,50 @@ from towhee.utils.log import engine_log
 
 
 def build_docker_image(
-        dc_pipeline: 'towhee.RuntimePipeline', image_name: str,
-        cuda_version: str, format_priority: list,
+        dc_pipeline: 'towhee.RuntimePipeline',
+        image_name: str,
+        cuda_version: str,
+        format_priority: list,
         parallelism: int = 8,
-        inference_server: str = 'triton'):
+        inference_server: str = 'triton'
+    ):
+    """
+    Build a docker image based on a RuntimePipeline.
+
+    Args:
+        dc_pipeline ('towhee.RuntimPipeline'):
+            The pipeline to build as a model in the docker image.
+        image_name (`str`):
+            The name of the docker image.
+        cuda_version (`str`):
+            Cuda version.
+        format_priority (`list`):
+            The priority order of the model format.
+        parallelism (`int`):
+            The parallel number.
+        inference_server (`str`):
+            The inference server.
+
+    Examples:
+        >>> import towhee
+        >>> from towhee import pipe, ops
+
+        >>> p = (
+        ...     pipe.input('url')
+        ...         .map('url', 'image', ops.image_decode.cv2_rgb())
+        ...         .map('image', 'vec', ops.image_embedding.timm(model_name='resnet50'))
+        ...         .output('vec')
+        ... )
+
+        >>> towhee.build_docker_image(
+        ...     dc_pipeline=p,
+        ...     image_name='clip:v1',
+        ...     cuda_version='11.7',
+        ...     format_priority=['onnx'],
+        ...     parallelism=4,
+        ...     inference_server='triton'
+        ... )
+    """
 
     server_config = {
         constant.FORMAT_PRIORITY : format_priority,
@@ -37,8 +77,47 @@ def build_docker_image(
         return False
 
 
-def build_pipeline_model(dc_pipeline: 'towhee.RuntimePipeline', model_root: str,
-                         format_priority: list, parallelism: int = 8, server: str = 'triton'):
+def build_pipeline_model(
+        dc_pipeline: 'towhee.RuntimePipeline',
+        model_root: str,
+        format_priority: list,
+        parallelism: int = 8,
+        server: str = 'triton'
+    ):
+    """
+    Build the pipeline as a model.
+
+    Args:
+        dc_pipeline ('towhee.RuntimePipeline'):
+            The piepline to build as a model.
+        model_root (`str`):
+            The model root path.
+        format_priority (`list`):
+            The priority order of the model format.
+        parallelism (`int`):
+            The parallel number.
+        server (`str`):
+            The server type.
+
+    Examples:
+        >>> import towhee
+        >>> from towhee import pipe, ops
+
+        >>> p = (
+        ...     pipe.input('url')
+        ...         .map('url', 'image', ops.image_decode.cv2_rgb())
+        ...         .map('image', 'vec', ops.image_embedding.timm(model_name='resnet50'))
+        ...         .output('vec')
+        ... )
+
+        >>> towhee.build_pipeline_model(
+        ...     dc_pipeline=p,
+        ...     model_root='models',
+        ...     format_priority=['onnx'],
+        ...     parallelism=4,
+        ...     server='triton'
+        ... )
+    """
 
     if server == 'triton':
         dag_repr = copy.deepcopy(dc_pipeline.dag_repr)
